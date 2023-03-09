251949fe2e492ced98479507d59d4d55c385eae2
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-03-09 20:08:19 +0100

    add more enums

--- shared/src/namespaces/EnumsNamespace.cpp
+++ shared/src/namespaces/EnumsNamespace.cpp
@@ -1,5 +1,6 @@
 #include "Namespace.h"
 #include "magic_enum/include/magic_enum.hpp"
+#include "cpp-sdk/SDK.h"
 
 template<typename T>
 static void GetEnumObject(js::LazyPropertyContext& ctx)
@@ -16,4 +17,11 @@ static void GetEnumObject(js::LazyPropertyContext& ctx)
 // clang-format off
 extern js::Namespace enumsNamespace("Enums", [](js::NamespaceTemplate& tpl) {
     tpl.StaticLazyProperty("BaseObjectType", GetEnumObject<alt::IBaseObject::Type>);
+    tpl.StaticLazyProperty("Permission", GetEnumObject<alt::Permission>);
+    tpl.StaticLazyProperty("PermissionState", GetEnumObject<alt::PermissionState>);
+    tpl.StaticLazyProperty("ExplosionType", GetEnumObject<alt::CExplosionEvent::ExplosionType>);
+    tpl.StaticLazyProperty("KeyState", GetEnumObject<alt::KeyState>);
+    tpl.StaticLazyProperty("BlipType", GetEnumObject<alt::IBlip::BlipType>);
+    tpl.StaticLazyProperty("ColShapeType", GetEnumObject<alt::IColShape::ColShapeType>);
+    tpl.StaticLazyProperty("VehicleModelType", GetEnumObject<alt::VehicleModelInfo::Type>);
 });
