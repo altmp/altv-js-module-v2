7d8e5cf859090bdf9f9857197d9a6ee7cf8a1400
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-03-11 22:19:05 +0100

    add pos and rot to vehicle ctor

--- server/src/classes/Vehicle.cpp
+++ server/src/classes/Vehicle.cpp
@@ -4,27 +4,19 @@
 static void Ctor(js::FunctionContext& ctx)
 {
     if(!ctx.CheckCtor()) return;
-    if(!ctx.CheckArgCount(1)) return;
+    if(!ctx.CheckArgCount(2, 3)) return;
 
     uint32_t model;
     if(!ctx.GetArgAsHash(0, model)) return;
+    alt::Position pos;
+    if(!ctx.GetArg(1, pos)) return;
+    alt::Rotation rot{ 0, 0, 0 };
+    if(ctx.GetArgCount() == 3 && !ctx.GetArg(2, rot)) return;
 
-    alt::IVehicle* vehicle = alt::ICore::Instance().CreateVehicle(model, { 0, 0, 0 }, { 0, 0, 0 });
+    alt::IVehicle* vehicle = alt::ICore::Instance().CreateVehicle(model, pos, rot);
     if(!ctx.Check(vehicle != nullptr, "Failed to create vehicle")) return;
 
     ctx.SetThisObject(vehicle);
-}
-
-static void GetID(js::FunctionContext& ctx)
-{
-    alt::IVehicle* vehicle = ctx.GetThisObject<alt::IVehicle>();
-    ctx.Return(vehicle->GetID());
-}
-
-static void GetModel(js::FunctionContext& ctx)
-{
-    alt::IVehicle* vehicle = ctx.GetThisObject<alt::IVehicle>();
-    ctx.Return(vehicle->GetModel());
 }
 
 // clang-format off
@@ -32,8 +24,4 @@ extern js::Class sharedVehicleClass;
 extern js::Class vehicleClass("Vehicle", &sharedVehicleClass, Ctor, [](js::ClassTemplate& tpl)
 {
     tpl.BindToType(alt::IBaseObject::Type::VEHICLE);
-
-    tpl.Method("getID", GetID);
-    tpl.Method("getModel", GetModel);
-    tpl.Property<alt::IEntity, &alt::IEntity::GetModel>("model");
 });
