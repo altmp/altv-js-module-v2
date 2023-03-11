21b7c582594697ed8359c7d5b83851693b27cb49
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-03-11 16:14:48 +0100

    add parent to namespace

--- shared/src/Namespace.h
+++ shared/src/Namespace.h
@@ -15,9 +15,11 @@ namespace js
     private:
         std::string name;
         NamespaceInitializationCallback initCb;
+        Namespace* parent = nullptr;
 
     public:
         Namespace(const std::string& _name, NamespaceInitializationCallback _cb) : name(_name), initCb(_cb) {}
+        Namespace(const std::string& _name, Namespace* _parent, NamespaceInitializationCallback _cb) : name(_name), parent(_parent), initCb(_cb) {}
 
         const std::string& GetName() const
         {
@@ -26,7 +28,7 @@ namespace js
 
         v8::Local<v8::ObjectTemplate> Initialize(v8::Isolate* isolate)
         {
-            NamespaceTemplate tpl(isolate, v8::ObjectTemplate::New(isolate));
+            NamespaceTemplate tpl(isolate, parent ? parent->Initialize(isolate) : v8::ObjectTemplate::New(isolate));
             initCb(tpl);
             return tpl.Get();
         }
