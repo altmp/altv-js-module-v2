8372a4cd7fcf8c3f69d4bcbdf683af82bba31e20
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-03-15 21:06:13 +0100

    add player.emit

--- server/src/classes/Player.cpp
+++ server/src/classes/Player.cpp
@@ -14,11 +14,34 @@ static void GetByID(js::FunctionContext& ctx)
         ctx.Return((alt::IBaseObject*)entity);
 }
 
+static void Emit(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckThis()) return;
+    if(!ctx.CheckArgCount(1, 32)) return;
+
+    alt::IPlayer* player = ctx.GetThisObject<alt::IPlayer>();
+
+    std::string eventName;
+    if(!ctx.GetArg(0, eventName)) return;
+
+    alt::MValueArgs args;
+    args.Reserve(ctx.GetArgCount() - 1);
+    alt::MValue val;
+    for(int i = 1; i < ctx.GetArgCount(); i++)
+    {
+        if(!ctx.GetArg(i, val)) continue;
+        args.Push(val);
+    }
+    alt::ICore::Instance().TriggerClientEvent(player, eventName, args);
+}
+
 // clang-format off
 extern js::Class sharedPlayerClass;
 extern js::Class playerClass("Player", &sharedPlayerClass, nullptr, [](js::ClassTemplate& tpl)
 {
     tpl.BindToType(alt::IBaseObject::Type::PLAYER);
+
+    tpl.Method("emit", &Emit);
 
     tpl.StaticFunction("getByID", &GetByID);
 });
