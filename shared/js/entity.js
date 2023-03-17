079df4eff56f17e27e7e6b8d10c7433daff324c5
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-03-17 13:50:39 +0100

    add Entity.all

--- shared/js/entity.js
+++ shared/js/entity.js
@@ -5,7 +5,8 @@ const { Event } = requireBinding("shared/events.js");
 /**
  * @type {Map<number, Set<unknown>>}
  */
-const entityAllMap = new Map();
+const entityAllMap = new Map(); // Stores a map of all entities by type
+const entityAllSet = new Set(); // Stores a set of all entities
 
 function addAllGetter(class_, type) {
     const set = new Set();
@@ -15,6 +16,7 @@ function addAllGetter(class_, type) {
 
 alt.Events.onBaseObjectCreate(({ object }) => {
     if (object instanceof alt.Entity) {
+        entityAllSet.add(object);
         const all = entityAllMap.get(object.type);
         if (all) all.add(object);
         else alt.logError("INTERNAL ERROR: Entity type", object.type, "missing from entityAllMap");
@@ -23,6 +25,7 @@ alt.Events.onBaseObjectCreate(({ object }) => {
 
 alt.Events.onBaseObjectRemove(({ object }) => {
     if (object instanceof alt.Entity) {
+        entityAllSet.delete(object);
         const all = entityAllMap.get(object.type);
         if (all) all.delete(object);
         else alt.logError("INTERNAL ERROR: Entity type", object.type, "missing from entityAllMap");
@@ -32,10 +35,13 @@ alt.Events.onBaseObjectRemove(({ object }) => {
 // Needed because base object events are called on next tick, and entities created from scripts
 // should be immediately accessible in .all
 export function addEntityToAll(entity) {
+    entityAllSet.add(entity);
     const all = entityAllMap.get(entity.type);
     if (all) all.add(entity);
 }
 
 // Register all getters
+alt.Entity.all = entityAllSet;
+
 addAllGetter(alt.Player, alt.Enums.BaseObjectType.PLAYER);
 addAllGetter(alt.Vehicle, alt.Enums.BaseObjectType.VEHICLE);
