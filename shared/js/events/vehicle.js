e0221e925673b32e7b51464b63263595eb75a37e
YannBcf <Yannbeats.pro@gmail.com>
2023-03-18 02:38:57 +0100

    add vehicle events

---
+++ shared/js/events/vehicle.js
@@ -0,0 +1,8 @@
+// clang-format off
+const { Event } = requireBinding("shared/events.js");
+
+Event.register(alt.Enums.EventType.VEHICLE_DESTROY, "VehicleDestroy");
+Event.register(alt.Enums.EventType.VEHICLE_ATTACH, "VehicleAttach");
+Event.register(alt.Enums.EventType.VEHICLE_DETACH, "VehicleDetach");
+Event.register(alt.Enums.EventType.NETOWNER_CHANGE, "NetOwnerChange");
+Event.register(alt.Enums.EventType.VEHICLE_DAMAGE, "VehicleDamage");
