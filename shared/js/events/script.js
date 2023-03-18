7e749753071947727bab1dad0d9c55107debbd74
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-03-18 19:44:07 +0100

    events cleanup

--- shared/js/events/script.js
+++ shared/js/events/script.js
@@ -4,11 +4,6 @@ Event.register(alt.Enums.EventType.SERVER_SCRIPT_EVENT, "ServerScriptEvent");
 Event.register(alt.Enums.EventType.CLIENT_SCRIPT_EVENT, "ClientScriptEvent");
 // TODO: change to a better name
 Event.register(alt.Enums.EventType.COLSHAPE_EVENT, "Colshape");
-Event.register(alt.Enums.EventType.WEAPON_DAMAGE_EVENT, "WeaponDamage");
 Event.register(alt.Enums.EventType.EXPLOSION_EVENT, "Explosion");
 Event.register(alt.Enums.EventType.FIRE_EVENT, "FireStart");
 Event.register(alt.Enums.EventType.START_PROJECTILE_EVENT, "ProjectileStart");
-Event.register(alt.Enums.EventType.SYNCED_META_CHANGE, "SyncedMetaChange");
-Event.register(alt.Enums.EventType.STREAM_SYNCED_META_CHANGE, "StreamSyncedMetaChange");
-Event.register(alt.Enums.EventType.GLOBAL_META_CHANGE, "GlobalMetaChange");
-Event.register(alt.Enums.EventType.GLOBAL_SYNCED_META_CHANGE, "GlobalSyncedMetaChange");
