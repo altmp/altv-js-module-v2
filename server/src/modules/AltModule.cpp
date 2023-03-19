4efc66ddf79ef5c45da11a8e4e537d59acda120f
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-03-19 22:42:43 +0100

    add more server alt module api

--- server/src/modules/AltModule.cpp
+++ server/src/modules/AltModule.cpp
@@ -1,6 +1,36 @@
 #include "Module.h"
 #include "Namespace.h"
 
+static void NetTimeGetter(js::PropertyContext& ctx)
+{
+    ctx.Return(alt::ICore::Instance().GetNetTime());
+}
+
+static void SetServerPassword(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckArgCount(1)) return;
+
+    std::string password;
+    if(!ctx.GetArg(0, password)) return;
+
+    alt::ICore::Instance().SetPassword(password);
+}
+
+static void HashServerPassword(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckArgCount(1)) return;
+
+    std::string password;
+    if(!ctx.GetArg(0, password)) return;
+
+    ctx.Return(alt::ICore::Instance().HashServerPassword(password));
+}
+
+static void StopServer(js::FunctionContext& ctx)
+{
+    alt::ICore::Instance().StopServer();
+}
+
 // clang-format off
 extern js::Class playerClass, vehicleClass, colShapeClass, checkpointClass;
 extern js::Namespace eventsNamespace;
@@ -8,6 +38,13 @@ static js::Module altModule("alt", "alt-shared", { &playerClass, &vehicleClass, 
 {
     module.StaticProperty("isClient", false);
     module.StaticProperty("isServer", true);
+    module.StaticProperty("rootDir", alt::ICore::Instance().GetRootDirectory());
+    module.StaticProperty("netTime", NetTimeGetter);
+
+    module.StaticFunction("setServerPassword", SetServerPassword);
+    module.StaticFunction("hashServerPasword", HashServerPassword);
+    module.StaticFunction("stopServer", StopServer);
+    // todo: get server config
 
     module.Namespace(eventsNamespace);
 
