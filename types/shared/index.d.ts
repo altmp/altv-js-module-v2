42c30215c62198618ef999c0043d6549d204856d
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-03-30 19:38:23 +0200

    more typings

--- types/shared/index.d.ts
+++ types/shared/index.d.ts
@@ -67,7 +67,7 @@ declare module "@altv/shared" {
 
     export namespace Enums {
         // See https://github.com/altmp/cpp-sdk/blob/master/objects/IBaseObject.h#L14
-        export enum BaseObjectType {
+        export const enum BaseObjectType {
             PLAYER,
             VEHICLE,
             PED,
@@ -91,7 +91,121 @@ declare module "@altv/shared" {
             PICKUP,
             SIZE
         }
+        export const enum BlipType {
+            VEHICLE = 1,
+			PED = 2,
+			OBJECT = 3,
+			DESTINATION = 4,
+			CONT = 5,
+			PICKUP_UNK = 6,
+			RADIUS = 7,
+			PICKUP = 8,
+			COP = 9,
+			AREA = 11,
+			GALLERY = 12,
+			PICKUP_OBJECT = 13
+        }
         // todo: add missing enums
+    }
+
+    export namespace Events {
+        export interface EventContext {
+            cancel(): void;
+            readonly isCancelled: boolean;
+        }
+        export interface Event<Context extends EventContext> {
+            (callback: (context: Context) => void): void;
+
+            remove(callback: Callback): void;
+            readonly listeners: ReadonlyArray<(context: Context) => void>;
+        };
+
+        interface ClientScriptEventContext extends EventContext {
+            readonly eventName: string;
+            readonly args: any[];
+            readonly player: Player | undefined;
+        }
+        interface ServerEventContext extends EventContext {
+            readonly eventName: string;
+            readonly args: any[];
+        }
+        interface ConsoleCommandEventContext extends EventContext {
+            readonly name: string;
+            readonly args: string[];
+        }
+        interface ResourceEventContext extends EventContext {
+            readonly resource: Resource;
+        }
+        interface PlayerAnimationChangeEventContext extends EventContext {
+            readonly player: Player;
+            readonly oldAnimDict: number;
+            readonly oldAnimName: number;
+            readonly newAnimDict: number;
+            readonly newAnimName: number;
+        }
+        interface LocalMetaChangeEventContext extends EventContext {
+            readonly player: Player;
+            readonly key: string;
+            readonly oldValue: any;
+            readonly newValue: any;
+        }
+        interface SyncedMetaChangeEventContext extends EventContext {
+            readonly entity: Entity;
+            readonly key: string;
+            readonly oldValue: any;
+            readonly newValue: any;
+        }
+        interface StreamSyncedMetaChangeEventContext extends EventContext {
+            readonly entity: Entity;
+            readonly key: string;
+            readonly oldValue: any;
+            readonly newValue: any;
+        }
+        interface GlobalMetaChangeEventContext extends EventContext {
+            readonly key: string;
+            readonly oldValue: any;
+            readonly newValue: any;
+        }
+        interface SyncedGlobalMetaChangeEventContext extends EventContext {
+            readonly key: string;
+            readonly oldValue: any;
+            readonly newValue: any;
+        }
+        interface BaseObjectCreateEventContext extends EventContext {
+            readonly object: BaseObject;
+        }
+        interface BaseObjectRemoveEventContext extends EventContext {
+            readonly object: BaseObject;
+        }
+        interface NetOwnerChangeEventContext extends EventContext {
+            readonly entity: Entity;
+            readonly oldOwner: Player;
+            readonly newOwner: Player;
+        }
+
+        export const onClientScriptEvent: Event<ClientScriptEventContext>;
+        export const onServerScriptEvent: Event<ServerEventContext>;
+
+        export const onConsoleCommand: Event<ConsoleCommandEventContext>;
+
+        export const onResourceStart: Event<ResourceEventContext>;
+        export const onResourceStop: Event<ResourceEventContext>;
+        export const onResourceError: Event<ResourceEventContext>;
+
+        export const onPlayerAnimationChange: Event<PlayerAnimationChangeEventContext>;
+
+        export const onLocalMetaChange: Event<LocalMetaChangeEventContext>;
+        export const onSyncedMetaChange: Event<SyncedMetaChangeEventContext>;
+        export const onStreamSyncedMetaChange: Event<StreamSyncedMetaChangeEventContext>;
+        export const onGlobalMetaChange: Event<GlobalMetaChangeEventContext>;
+        export const onSyncedGlobalMetaChange: Event<SyncedGlobalMetaChangeEventContext>;
+
+        export const onBaseObjectCreate: Event<BaseObjectCreateEventContext>;
+        export const onBaseObjectRemove: Event<BaseObjectRemoveEventContext>;
+
+        export const onNetOwnerChange: Event<NetOwnerChangeEventContext>;
+
+        export function emit(name: string, ...args: any[]): void;
     }
 
     export namespace PointBlip {}
@@ -241,18 +355,99 @@ declare module "@altv/shared" {
     }
 
     export class Player extends Entity {
+        get name(): string;
+        get health(): number;
+        get maxHealth(): number;
+        get currentWeaponComponents(): ReadonlyArray<number>;
+        get currentWeaponTintIndex(): number;
+        get currentWeapon(): number;
+        get isDead(): boolean;
+        get isJumping(): boolean;
+        get isAiming(): boolean;
+        get isShooting(): boolean;
+        get isReloading(): boolean;
+        get armour(): number;
+        get maxArmour(): number;
+        get moveSpeed(): number;
+        get aimPos(): Vector3;
+        get headRotation(): Vector3;
+        get isInVehicle(): boolean;
+        get vehicle(): Vehicle | null;
+        get seat(): number;
+        get entityAimingAt(): Entity | null;
+        get entityAimOffset(): Vector3;
+        get isFlashlightActive(): boolean;
+        get isSuperJumpEnabled(): boolean;
+        get isCrouching(): boolean;
+        get isStealthy(): boolean;
+        get currentAnimationDict(): number;
+        get currentAnimationName(): number;
+        get isSpawned(): boolean;
+        get forwardSpeed(): number;
+        get strafeSpeed(): number;
 
+        getWeaponTintIndex(weapon: number | string): number;
+        hasWeaponComponent(weapon: number | string, component: number | string): boolean;
     }
 
     export class Vehicle extends Entity {
-
+        // todo: add missing api
     }
 
     export class Blip extends WorldObject {
+        get id(): number;
+        get isGlobal(): boolean;
+        get target(): Entity | null;
+        get isAttached(): boolean;
+        get attachedTo(): Entity | null;
+        get blipType(): Enums.BlipType;
 
+        scale: Vector2;
+        display: number;
+        sprite: number;
+        color: number;
+        secondaryColor: RGBA;
+        alpha: number;
+        flashTimer: number;
+        flashInterval: number;
+        friendly: boolean;
+        route: boolean;
+        bright: boolean;
+        number: number;
+        showCone: boolean;
+        flashes: boolean;
+        flashesAlternate: boolean;
+        shortRange: boolean;
+        priority: number;
+        rotation: number;
+        gxtName: string;
+        name: string;
+        routeColor: RGBA;
+        pulse: boolean;
+        missionCreator: boolean;
+        tickVisible: boolean;
+        headingIndicatorVisible: boolean;
+        outlineIndicatorVisible: boolean;
+        friendIndicatorVisible: boolean;
+        crewIndicatorVisible: boolean;
+        category: number;
+        highDetail: boolean;
+        shrinked: boolean;
+
+        attachTo(entity: Entity): void;
+        fade(opacity: number, duration: number): void;
     }
 
     export class NetworkObject extends Entity {
+        get alpha(): number;
+        get textureVariation(): number;
+        get lodDistance(): number;
+    }
 
+    export class Ped extends Entity {
+        get health(): number;
+        get maxHealth(): number;
+        get armour(): number;
+        get currentWeapon(): number;
     }
 }
