2037b71bf744f0de003e156735b74ca17aac9e25
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-04-23 16:11:30 +0200

    server: Fix attachTo

--- server/src/classes/Entity.cpp
+++ server/src/classes/Entity.cpp
@@ -21,6 +21,63 @@ static void ResetNetOwner(js::FunctionContext& ctx)
     entity->SetNetworkOwner(nullptr, disableMigration);
 }
 
+static void AttachTo(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckThis()) return;
+    if(!ctx.CheckArgCount(7)) return;
+    alt::IEntity* entity = ctx.GetThisObject<alt::IEntity>();
+
+    alt::IEntity* target;
+    if(!ctx.GetArg(0, target)) return;
+
+    if(!ctx.CheckArgType(1, { js::Type::NUMBER, js::Type::STRING })) return;
+
+    if(ctx.GetArgType(1) == js::Type::NUMBER)
+    {
+        int16_t otherBoneIndex;
+        if(!ctx.GetArg(1, otherBoneIndex)) return;
+
+        int16_t myBoneIndex;
+        if(!ctx.GetArg(2, myBoneIndex)) return;
+
+        alt::Position pos;
+        if(!ctx.GetArg(3, pos)) return;
+
+        alt::Rotation rot;
+        if(!ctx.GetArg(4, rot)) return;
+
+        bool collision;
+        if(!ctx.GetArg(5, collision)) return;
+
+        bool noFixedRot;
+        if(!ctx.GetArg(6, noFixedRot)) return;
+
+        entity->AttachToEntity(target, otherBoneIndex, myBoneIndex, pos, rot, collision, noFixedRot);
+    }
+    else
+    {
+        std::string otherBoneName;
+        if(!ctx.GetArg(1, otherBoneName)) return;
+
+        std::string myBoneName;
+        if(!ctx.GetArg(2, myBoneName)) return;
+
+        alt::Position pos;
+        if(!ctx.GetArg(3, pos)) return;
+
+        alt::Rotation rot;
+        if(!ctx.GetArg(4, rot)) return;
+
+        bool collision;
+        if(!ctx.GetArg(5, collision)) return;
+
+        bool noFixedRot;
+        if(!ctx.GetArg(6, noFixedRot)) return;
+
+        entity->AttachToEntity(target, otherBoneName, myBoneName, pos, rot, collision, noFixedRot);
+    }
+}
+
 static void SyncedMetaSetter(js::DynamicPropertyContext<v8::Value>& ctx)
 {
     if(!ctx.CheckParent()) return;
@@ -76,8 +133,7 @@ extern js::Class entityClass("Entity", &sharedEntityClass, nullptr, [](js::Class
     tpl.MethodEx<&alt::IEntity::SetNetworkOwner>("setNetOwner");
     tpl.Method("resetNetOwner", ResetNetOwner);
 
-    tpl.MethodEx<static_cast<void(alt::IEntity:: *)(alt::IEntity *, int16_t, int16_t, alt::Position, alt::Rotation, bool, bool)>(&alt::IEntity::AttachToEntity)>("attachTo");
-    tpl.MethodEx<static_cast<void(alt::IEntity:: *)(alt::IEntity *, const std::string&, const std::string&, alt::Position, alt::Rotation, bool, bool)>(&alt::IEntity::AttachToEntity)>("attachTo");
+    tpl.Method("attachTo", &AttachTo);
     tpl.MethodEx<&alt::IEntity::Detach>("detach");
 
     tpl.Property<alt::IEntity, bool, &alt::IEntity::GetVisible, &alt::IEntity::SetVisible>("visible");
