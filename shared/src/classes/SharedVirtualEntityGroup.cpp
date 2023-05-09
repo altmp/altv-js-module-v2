549d70d44d8bb342745fdc3fbb1088559ccf7d9f
xshady <54737754+xxshady@users.noreply.github.com>
2023-05-09 21:07:28 +0300

    shared: Rename VirtualEntityGroup property

--- shared/src/classes/SharedVirtualEntityGroup.cpp
+++ shared/src/classes/SharedVirtualEntityGroup.cpp
@@ -6,5 +6,5 @@ extern js::Class baseObjectClass;
 extern js::Class sharedVirtualEntityGroupClass("SharedVirtualEntityGroup", &baseObjectClass, nullptr, [](js::ClassTemplate& tpl)
 {
     tpl.LazyProperty<&alt::IVirtualEntityGroup::GetID>("id");
-    tpl.LazyProperty<&alt::IVirtualEntityGroup::GetStreamingRangeLimit>("streamingRangeLimit");
+    tpl.LazyProperty<&alt::IVirtualEntityGroup::GetStreamingRangeLimit>("maxStreamedEntityCount");
 });
