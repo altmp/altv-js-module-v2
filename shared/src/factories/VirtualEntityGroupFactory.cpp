d725f6a55471c94e45662820ad8f58bf2c949111
xshady <54737754+xxshady@users.noreply.github.com>
2023-05-09 20:56:06 +0300

    server: Rename streamingRangeLimit to something less confusing

--- shared/src/factories/VirtualEntityGroupFactory.cpp
+++ shared/src/factories/VirtualEntityGroupFactory.cpp
@@ -2,7 +2,7 @@
 
 // clang-format off
 static js::FactoryHandler virtualEntityGroupFactory(alt::IBaseObject::Type::VIRTUAL_ENTITY_GROUP, [](js::Object& args) -> alt::IBaseObject* {
-    uint32_t streamingRangeLimit;
-    if(!args.Get("streamingRangeLimit", streamingRangeLimit)) return nullptr;
-    return alt::ICore::Instance().CreateVirtualEntityGroup(streamingRangeLimit);
+    uint32_t maxStreamedEntityCount;
+    if(!args.Get("maxStreamedEntityCount", maxStreamedEntityCount)) return nullptr;
+    return alt::ICore::Instance().CreateVirtualEntityGroup(maxStreamedEntityCount);
 });
