2f8fcb8a44b42de97b3a3640b5a8771dcce61a70
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-05-28 21:09:27 +0200

    server: Add metric class

---
+++ server/src/classes/Metric.cpp
@@ -0,0 +1,93 @@
+#include "Class.h"
+#include "cpp-sdk/ICore.h"
+#include "CNodeResource.h"
+
+static void Constructor(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckCtor()) return;
+    if(!ctx.CheckArgCount(1, 2)) return;
+    CNodeResource* resource = ctx.GetResource<CNodeResource>();
+
+    std::string name;
+    if(!ctx.GetArg(0, name)) return;
+
+    alt::Metric::Type type = ctx.GetArg<alt::Metric::Type>(1, alt::Metric::Type::METRIC_TYPE_GAUGE);
+
+    alt::Metric* metric = resource->RegisterMetric(name, type);
+    ctx.SetExtraInternalFieldValue(metric);
+}
+
+static void NameGetter(js::LazyPropertyContext& ctx)
+{
+    if(!ctx.CheckExtraInternalFieldValue()) return;
+    alt::Metric* metric = ctx.GetExtraInternalFieldValue<alt::Metric>();
+
+    ctx.Return(metric->name);
+}
+
+static void ValueGetter(js::PropertyContext& ctx)
+{
+    if(!ctx.CheckExtraInternalFieldValue()) return;
+    alt::Metric* metric = ctx.GetExtraInternalFieldValue<alt::Metric>();
+
+    ctx.Return(metric->value);
+}
+
+static void ValueSetter(js::PropertyContext& ctx)
+{
+    if(!ctx.CheckExtraInternalFieldValue()) return;
+    alt::Metric* metric = ctx.GetExtraInternalFieldValue<alt::Metric>();
+
+    uint64_t value;
+    if(!ctx.GetValue(value)) return;
+
+    metric->SetValue(value);
+}
+
+static void ValidGetter(js::PropertyContext& ctx)
+{
+    alt::Metric* metric = ctx.GetExtraInternalFieldValue<alt::Metric>();
+
+    ctx.Return(metric != nullptr && metric->exists == true);
+}
+
+static void Begin(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckExtraInternalFieldValue()) return;
+    alt::Metric* metric = ctx.GetExtraInternalFieldValue<alt::Metric>();
+
+    metric->Begin();
+}
+
+static void End(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckExtraInternalFieldValue()) return;
+    alt::Metric* metric = ctx.GetExtraInternalFieldValue<alt::Metric>();
+
+    metric->End();
+}
+
+static void Destroy(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckExtraInternalFieldValue()) return;
+    alt::Metric* metric = ctx.GetExtraInternalFieldValue<alt::Metric>();
+    CNodeResource* resource = ctx.GetResource<CNodeResource>();
+
+    metric->Destroy();
+    resource->UnregisterMetric(metric);
+
+    ctx.SetExtraInternalFieldValue(nullptr);
+}
+
+// clang-format off
+extern js::Class metricClass("Metric", nullptr, Constructor, [](js::ClassTemplate& tpl)
+{
+    tpl.LazyProperty("name", NameGetter);
+
+    tpl.Property("value", ValueGetter, ValueSetter);
+    tpl.Property("valid", ValidGetter);
+
+    tpl.Method("begin", Begin);
+    tpl.Method("end", End);
+    tpl.Method("destroy", Destroy);
+}, true);
--- server/src/modules/AltModule.cpp
+++ server/src/modules/AltModule.cpp
@@ -136,9 +136,9 @@ static void GetClosestEntities(js::FunctionContext& ctx)
 }
 
 // clang-format off
-extern js::Class playerClass, vehicleClass, colShapeClass, checkpointClass, pedClass, networkObjectClass, voiceChannelClass, blipClass, virtualEntityClass, virtualEntityGroupClass;
+extern js::Class playerClass, vehicleClass, colShapeClass, checkpointClass, pedClass, networkObjectClass, voiceChannelClass, blipClass, virtualEntityClass, virtualEntityGroupClass, metricClass;
 extern js::Namespace eventsNamespace, pedModelInfoNamespace, vehicleModelInfoNamespace;
-static js::Module altModule("@altv/server", "@altv/shared", { &playerClass, &vehicleClass, &colShapeClass, &checkpointClass, &pedClass, &networkObjectClass, &voiceChannelClass, &blipClass, &virtualEntityClass, &virtualEntityGroupClass }, [](js::ModuleTemplate& module)
+static js::Module altModule("@altv/server", "@altv/shared", { &playerClass, &vehicleClass, &colShapeClass, &checkpointClass, &pedClass, &networkObjectClass, &voiceChannelClass, &blipClass, &virtualEntityClass, &virtualEntityGroupClass, &metricClass }, [](js::ModuleTemplate& module)
 {
     module.StaticProperty("isClient", false);
     module.StaticProperty("isServer", true);
