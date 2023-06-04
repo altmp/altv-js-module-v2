8237094fcc1bd3f2346f41c82e02cd8ac7078d50
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-06-04 13:14:10 +0200

    client: Add local player getter

--- client/src/classes/Player.cpp
+++ client/src/classes/Player.cpp
@@ -1,8 +1,15 @@
 #include "Class.h"
 
+static void LocalGetter(js::LazyPropertyContext& ctx)
+{
+    ctx.Return(alt::ICore::Instance().GetLocalPlayer());
+}
+
 // clang-format off
 extern js::Class sharedPlayerClass;
 extern js::Class playerClass("Player", &sharedPlayerClass, nullptr, [](js::ClassTemplate& tpl)
 {
     tpl.BindToType(alt::IBaseObject::Type::PLAYER);
+
+    tpl.StaticLazyProperty("local", LocalGetter);
 });
