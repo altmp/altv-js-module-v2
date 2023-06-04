124464cfe1f2f71347095affd12361296dbb3134
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-06-04 13:21:02 +0200

    client: Add vehicle, ped and network object class

---
+++ client/src/classes/Ped.cpp
@@ -0,0 +1,23 @@
+#include "Class.h"
+#include "interfaces/IResource.h"
+#include "cpp-sdk/ICore.h"
+
+static void GetByID(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckArgCount(1)) return;
+
+    uint16_t id;
+    if(!ctx.GetArg(0, id)) return;
+
+    alt::IBaseObject* entity = alt::ICore::Instance().GetBaseObjectByID(alt::IBaseObject::Type::PED, id);
+    ctx.Return(entity);
+}
+
+// clang-format off
+extern js::Class sharedPedClass;
+extern js::Class pedClass("Ped", &sharedPedClass, nullptr, [](js::ClassTemplate& tpl)
+{
+    tpl.BindToType(alt::IBaseObject::Type::PED);
+
+    tpl.StaticFunction("getByID", &GetByID);
+});
---
+++ client/src/classes/Vehicle.cpp
@@ -0,0 +1,21 @@
+#include "Class.h"
+
+static void GetByID(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckArgCount(1)) return;
+
+    uint16_t id;
+    if(!ctx.GetArg(0, id)) return;
+
+    alt::IBaseObject* entity = alt::ICore::Instance().GetBaseObjectByID(alt::IBaseObject::Type::VEHICLE, id);
+    ctx.Return(entity);
+}
+
+// clang-format off
+extern js::Class sharedVehicleClass;
+extern js::Class vehicleClass("Vehicle", &sharedVehicleClass, nullptr, [](js::ClassTemplate& tpl)
+{
+    tpl.BindToType(alt::IBaseObject::Type::VEHICLE);
+
+    tpl.StaticFunction("getByID", &GetByID);
+});
