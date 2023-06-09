4ef59d869a13a881c2cca8ab11cb5f08592d14d2
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-06-10 00:06:21 +0200

    client: Add missing Player.getByID

--- client/src/classes/Player.cpp
+++ client/src/classes/Player.cpp
@@ -5,6 +5,17 @@ static void LocalGetter(js::LazyPropertyContext& ctx)
     ctx.Return(alt::ICore::Instance().GetLocalPlayer());
 }
 
+static void GetByID(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckArgCount(1)) return;
+
+    uint32_t id;
+    if(!ctx.GetArg(0, id)) return;
+
+    alt::IBaseObject* entity = alt::ICore::Instance().GetBaseObjectByID(alt::IBaseObject::Type::PLAYER, id);
+    ctx.Return(entity);
+}
+
 // clang-format off
 extern js::Class sharedPlayerClass;
 extern js::Class playerClass("Player", &sharedPlayerClass, nullptr, [](js::ClassTemplate& tpl)
@@ -12,4 +23,6 @@ extern js::Class playerClass("Player", &sharedPlayerClass, nullptr, [](js::Class
     tpl.BindToType(alt::IBaseObject::Type::PLAYER);
 
     tpl.StaticLazyProperty("local", LocalGetter);
+
+    tpl.StaticFunction("getByID", GetByID);
 });
