8e7e31860c4094b3a92a89113e1aa4590d4bec02
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-06-21 22:19:37 +0200

    client: Implement streamedIn getter

---
+++ client/js/entity.js
@@ -0,0 +1,79 @@
+// Entity streamed-in stores
+/**
+ * @type {Map<number, Set<unknown>>}
+ */
+const entityStreamedInMap = new Map(); // Stores a map of all streamed-in entities by type
+const entityStreamedInSet = new Set(); // Stores a set of all streamed-in entities
+
+/**
+ * @param {unknown} class_
+ * @param {number | number[]} types
+ */
+function addStreamedInGetter(class_, types) {
+    if (!class_) return;
+    if (!Array.isArray(types)) types = [types];
+
+    const sets = [];
+    for (const type of types) {
+        const set = new Set();
+        entityStreamedInMap.set(type, set);
+        sets.push(set);
+    }
+
+    Object.defineProperties(class_, {
+        streamedIn: {
+            get: () => {
+                if (class_.__streamedInDirty) {
+                    class_.__streamedInCached = sets.flatMap((set) => Array.from(set));
+                    class_.__streamedInDirty = false;
+                }
+                return class_.__streamedInCached;
+            }
+        },
+        __streamedInDirty: {
+            value: false,
+            enumerable: false,
+            writable: true,
+            configurable: false
+        },
+        __streamedInCached: {
+            value: [],
+            enumerable: false,
+            writable: true,
+            configurable: false
+        }
+    });
+}
+
+function addEntityToStreamedIn(entity) {
+    entityStreamedInSet.add(entity);
+    const streamedIn = entityStreamedInMap.get(entity.type);
+    streamedIn?.add(entity);
+    entity.constructor.__streamedInDirty = true;
+}
+function removeEntityFromStreamedIn(entity) {
+    entityStreamedInSet.delete(entity);
+    const streamedIn = entityStreamedInMap.get(entity.type);
+    streamedIn?.delete(entity);
+    entity.constructor.__streamedInDirty = true;
+}
+
+alt.Events.onGameEntityCreate(({ entity }) => {
+    addEntityToStreamedIn(entity);
+});
+
+alt.Events.onGameEntityDestroy(({ entity }) => {
+    removeEntityFromStreamedIn(entity);
+});
+
+// Register streamedIn getters
+Object.defineProperty(alt.Entity, "streamedIn", {
+    get: () => Array.from(entityStreamedInSet)
+});
+
+addStreamedInGetter(alt.Player, alt.Enums.BaseObjectType.PLAYER);
+addStreamedInGetter(alt.Vehicle, [alt.Enums.BaseObjectType.VEHICLE, alt.Enums.BaseObjectType.LOCAL_VEHICLE]);
+addStreamedInGetter(alt.Ped, alt.Enums.BaseObjectType.PED);
+addStreamedInGetter(alt.NetworkObject, alt.Enums.BaseObjectType.NETWORK_OBJECT);
+
+// todo: add already streamed in entities?
