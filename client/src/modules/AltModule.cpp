983c9fb59206061a54c5aa205813a34fcff8fb62
Davidos <david.solobub@gmail.com>
2023-06-23 22:49:41 +0300

    client: Add missing textLabelClass to alt module

--- client/src/modules/AltModule.cpp
+++ client/src/modules/AltModule.cpp
@@ -4,7 +4,7 @@
 extern js::Class playerClass, localPlayerClass, vehicleClass, pedClass, networkObjectClass, bufferClass, audioClass, audioFilterClass, blipClass, textLabelClass;
 extern js::Namespace eventsNamespace, discordNamespace, voiceNamespace;
 static js::Module altModule("@altv/client", "@altv/shared",
-    { &playerClass, &localPlayerClass, &vehicleClass, &pedClass, &networkObjectClass, &bufferClass, &audioClass, &audioFilterClass, &blipClass },
+    { &playerClass, &localPlayerClass, &vehicleClass, &pedClass, &networkObjectClass, &bufferClass, &audioClass, &audioFilterClass, &blipClass, &textLabelClass },
 [](js::ModuleTemplate& module) {
     module.StaticProperty("isClient", true);
     module.StaticProperty("isServer", false);
