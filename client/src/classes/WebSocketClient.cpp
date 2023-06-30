4c74fcae5869b19281d3f0eb35dc1cb2a657950d
Davidos <38360572+xDavidos@users.noreply.github.com>
2023-06-30 23:53:31 +0300

    client: Add WebSocketClient class (#45)
    * client: Add WebSocketClient class
    
    * client: fix type

---
+++ client/src/classes/WebSocketClient.cpp
@@ -0,0 +1,54 @@
+#include "Class.h"
+#include "helpers/Buffer.h"
+
+static void Send(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckThis()) return;
+    if(!ctx.CheckArgCount(1)) return;
+    if(!ctx.CheckArgType(0, { js::Type::STRING, js::Type::TYPED_ARRAY, js::Type::ARRAY_BUFFER })) return;
+    alt::IWebSocketClient* websocket = ctx.GetThisObject<alt::IWebSocketClient>();
+
+    if(ctx.GetArgType(0) == js::Type::STRING)
+    {
+        std::string msg;
+        if(!ctx.GetArg(0, msg)) return;
+
+        ctx.Return(websocket->Send(msg));
+    }
+    else if (ctx.GetArgType(0) == js::Type::TYPED_ARRAY)
+    {
+        js::Buffer* buffer;
+        if(!ctx.GetArg(0, buffer)) return;
+
+        ctx.Return(websocket->SendBinary());
+    }
+    else if (ctx.GetArgType(0) == js::Type::ARRAY_BUFFER)
+    {
+        js::Buffer* buffer;
+        if(!ctx.GetArg(0, buffer)) return;
+
+        ctx.Return(websocket->SendBinary());
+    }
+}
+
+// clang-format off
+extern js::Class baseObjectClass;
+extern js::Class webSocketClientClass("WebSocketClient", &baseObjectClass, [](js::ClassTemplate& tpl) {
+    tpl.BindToType(alt::IBaseObject::Type::WEBSOCKET_CLIENT);
+
+    tpl.Property<&alt::IWebSocketClient::GetUrl, &alt::IWebSocketClient::SetUrl>("url");
+    tpl.Property<&alt::IWebSocketClient::IsAutoReconnectEnabled, &alt::IWebSocketClient::SetAutoReconnectEnabled>("autoReconnect");
+    tpl.Property<&alt::IWebSocketClient::IsPerMessageDeflateEnabled, &alt::IWebSocketClient::SetPerMessageDeflateEnabled>("perMessageDeflate");
+    tpl.Property<&alt::IWebSocketClient::GetPingInterval, &alt::IWebSocketClient::SetPingInterval>("pingInterval");
+    tpl.Property<&alt::IWebSocketClient::GetReadyState>("readyState");
+
+    tpl.Method<&alt::IWebSocketClient::Start>("start");
+    tpl.Method<&alt::IWebSocketClient::Stop>("stop");
+    tpl.Method("send", Send);
+    tpl.Method<&alt::IWebSocketClient::AddSubProtocol>("addSubProtocol");
+    tpl.Method<&alt::IWebSocketClient::GetSubProtocols>("GetSubProtocols");
+    tpl.Method<&alt::IWebSocketClient::SetExtraHeader>("setExtraHeader");
+    tpl.Method<&alt::IWebSocketClient::GetExtraHeaders>("getExtraHeaders");
+
+    tpl.GetByID<alt::IBaseObject::Type::WEBSOCKET_CLIENT>();
+});
---
+++ client/src/factories/WebSocketClientFactory.cpp
@@ -0,0 +1,10 @@
+#include "FactoryHandler.h"
+#include "interfaces/IResource.h"
+
+// clang-format off
+static js::FactoryHandler webSocketClientCFactory(alt::IBaseObject::Type::WEBSOCKET_CLIENT, [](js::Object& args) -> alt::IBaseObject* {
+    std::string url;
+    if(!args.Get("url", url)) return nullptr;
+
+    return alt::ICore::Instance().CreateWebSocketClient(url, args.GetResource()->GetResource());
+});
