1091f21950f9e710e941cea13fb968569eecb349
Davidos <38360572+xDavidos@users.noreply.github.com>
2023-07-02 16:00:52 +0300

    client: Add WorldObject events (#48)

---
+++ client/src/events/WorldObjectEvent.cpp
@@ -0,0 +1,21 @@
+#include "Event.h"
+
+// clang-format off
+static js::Event worldObjectPositionChangeEvent(alt::CEvent::Type::WORLD_OBJECT_POSITION_CHANGE, [](const alt::CEvent* ev, js::Event::EventArgs& args)
+{
+    auto e = static_cast<const alt::CWorldObjectPositionChangeEvent*>(ev);
+    args.Set("object", e->GetWorldObject());
+    args.Set("oldPosition", e->GetOldPosition());
+});
+
+static js::Event worldObjectStreamInEvent(alt::CEvent::Type::WORLD_OBJECT_STREAM_IN, [](const alt::CEvent* ev, js::Event::EventArgs& args)
+{
+    auto e = static_cast<const alt::CWorldObjectStreamInEvent*>(ev);
+    args.Set("object", e->GetWorldObject());
+});
+
+static js::Event worldObjectStreamOutEvent(alt::CEvent::Type::WORLD_OBJECT_STREAM_OUT, [](const alt::CEvent* ev, js::Event::EventArgs& args)
+{
+    auto e = static_cast<const alt::CWorldObjectStreamOutEvent*>(ev);
+    args.Set("object", e->GetWorldObject());
+});
