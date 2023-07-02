db36ecc64a93b78c06f84a83027bf9e3120bcc24
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-07-02 23:16:31 +0200

    client: Add VirtualEntity and VirtualEntityGroup classes

---
+++ client/src/classes/VirtualEntity.cpp
@@ -0,0 +1,11 @@
+#include "Class.h"
+#include "cpp-sdk/ICore.h"
+
+// clang-format off
+extern js::Class sharedVirtualEntityClass;
+extern js::Class virtualEntityClass("VirtualEntity", &sharedVirtualEntityClass, nullptr, [](js::ClassTemplate& tpl)
+{
+    tpl.BindToType(alt::IBaseObject::Type::VIRTUAL_ENTITY);
+
+    tpl.Property<&alt::IVirtualEntity::IsStreamedIn>("isStreamedIn");
+});
--- client/src/modules/AltModule.cpp
+++ client/src/modules/AltModule.cpp
@@ -1,10 +1,16 @@
 #include "Module.h"
 
 // clang-format off
-extern js::Class playerClass, localPlayerClass, vehicleClass, pedClass, networkObjectClass, audioClass, audioFilterClass, blipClass, textLabelClass, checkpointClass, webViewClass, fontClass, localPedClass, localVehicleClass, rmlDocumentClass, rmlElementClass, objectClass, webSocketClientClass, mapZoomDataClass;
+extern js::Class playerClass, localPlayerClass, vehicleClass, pedClass, networkObjectClass,
+                audioClass, audioFilterClass, blipClass, textLabelClass, checkpointClass, webViewClass, fontClass,
+                localPedClass, localVehicleClass, rmlDocumentClass, rmlElementClass, objectClass, webSocketClientClass,
+                mapZoomDataClass, virtualEntityClass, virtualEntityGroupClass;
 extern js::Namespace eventsNamespace, discordNamespace, voiceNamespace, localStorageNamespace, statsNamespace;
 static js::Module altModule("@altv/client", "@altv/shared",
-    { &playerClass, &localPlayerClass, &vehicleClass, &pedClass, &networkObjectClass, &audioClass, &audioFilterClass, &blipClass, &textLabelClass, &checkpointClass, &webViewClass, &fontClass, &localPedClass, &localVehicleClass, &rmlDocumentClass, &rmlElementClass, &objectClass, &webSocketClientClass, &mapZoomDataClass },
+    { &playerClass, &localPlayerClass, &vehicleClass, &pedClass, &networkObjectClass,
+    &audioClass, &audioFilterClass, &blipClass, &textLabelClass, &checkpointClass, &webViewClass, &fontClass,
+    &localPedClass, &localVehicleClass, &rmlDocumentClass, &rmlElementClass, &objectClass, &webSocketClientClass,
+    &mapZoomDataClass, &virtualEntityClass, &virtualEntityGroupClass },
 [](js::ModuleTemplate& module) {
     module.StaticProperty("isClient", true);
     module.StaticProperty("isServer", false);
