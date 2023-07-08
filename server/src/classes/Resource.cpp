a60a93a1e057a21e2d5aa128f9e80976412ff2c8
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-07-08 14:17:33 +0200

    server: Add missing API to Resource class

--- server/src/classes/Resource.cpp
+++ server/src/classes/Resource.cpp
@@ -1,6 +1,47 @@
 #include "Class.h"
 #include "interfaces/IAltResource.h"
 
+static void ClientTypeGetter(js::PropertyContext& ctx)
+{
+    if(!ctx.CheckExtraInternalFieldValue()) return;
+
+    alt::IResource* resource = ctx.GetExtraInternalFieldValue<alt::IResource>();
+    ctx.Return(resource->GetClientType());
+}
+
+static void ClientMainGetter(js::PropertyContext& ctx)
+{
+    if(!ctx.CheckExtraInternalFieldValue()) return;
+
+    alt::IResource* resource = ctx.GetExtraInternalFieldValue<alt::IResource>();
+    ctx.Return(resource->GetClientMain());
+}
+
+static void ClientFilesGetter(js::PropertyContext& ctx)
+{
+    if(!ctx.CheckExtraInternalFieldValue()) return;
+
+    alt::IResource* resource = ctx.GetExtraInternalFieldValue<alt::IResource>();
+    ctx.Return(resource->GetClientFiles());
+}
+
+static void GetMatchedFiles(js::FunctionContext& ctx)
+{
+    if(!ctx.CheckArgCount(1, 32)) return;
+
+    std::vector<std::string> patterns;
+    patterns.reserve(ctx.GetArgCount());
+    for(int i = 0; i < ctx.GetArgCount(); ++i)
+    {
+        std::string pattern;
+        if(!ctx.GetArg(i, pattern)) return;
+        patterns.push_back(pattern);
+    }
+
+    alt::IResource* resource = ctx.GetExtraInternalFieldValue<alt::IResource>();
+    ctx.Return(resource->GetMatchedFiles(patterns));
+}
+
 static void Current(js::PropertyContext& ctx)
 {
     js::IAltResource* resource = ctx.GetResource<js::IAltResource>();
@@ -57,12 +98,16 @@ static void Restart(js::FunctionContext& ctx)
 extern js::Class sharedResourceClass;
 extern js::Class resourceClass("Resource", &sharedResourceClass, nullptr, [](js::ClassTemplate& tpl)
 {
+    tpl.Property("clientType", ClientTypeGetter);
+    tpl.Property("clientMain", ClientMainGetter);
+    tpl.Property("clientFiles", ClientFilesGetter);
+
+    tpl.Method("getMatchedFiles", GetMatchedFiles);
+
     tpl.StaticProperty("current", Current);
     tpl.StaticProperty("all", All);
 
     tpl.StaticFunction("start", Start);
     tpl.StaticFunction("stop", Stop);
     tpl.StaticFunction("restart", Restart);
-
-    // todo: add missing properties
 }, true);
