b27abb303d1029b4786be79b6be0091796cad80b
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-07-27 16:18:20 +0200

    client: Add player.filter

--- client/src/classes/Player.cpp
+++ client/src/classes/Player.cpp
@@ -1,5 +1,29 @@
 #include "Class.h"
 
+static void FilterGetter(js::PropertyContext& ctx)
+{
+    if(!ctx.CheckThis()) return;
+    alt::IPlayer* player = ctx.GetThisObject<alt::IPlayer>();
+
+    ctx.Return(player->GetFilter());
+}
+
+static void FilterSetter(js::PropertyContext& ctx)
+{
+    if(!ctx.CheckThis()) return;
+    if(!ctx.CheckValueType({ js::Type::BASE_OBJECT, js::Type::NULL_TYPE })) return;
+    alt::IPlayer* player = ctx.GetThisObject<alt::IPlayer>();
+
+    if(ctx.GetValueType() == js::Type::BASE_OBJECT)
+    {
+        alt::IAudioFilter* filter;
+        if(!ctx.GetValue(filter)) return;
+        player->AddFilter(filter);
+    }
+    else
+        player->RemoveFilter();
+}
+
 static void LocalGetter(js::LazyPropertyContext& ctx)
 {
     ctx.Return(alt::ICore::Instance().GetLocalPlayer());
@@ -15,6 +39,7 @@ extern js::Class playerClass("Player", &sharedPlayerClass, nullptr, [](js::Class
     tpl.Property<&alt::IPlayer::GetMicLevel>("micLevel");
     tpl.Property<&alt::IPlayer::GetSpatialVolume, &alt::IPlayer::SetSpatialVolume>("spatialVolume");
     tpl.Property<&alt::IPlayer::GetNonSpatialVolume, &alt::IPlayer::SetNonSpatialVolume>("nonSpatialVolume");
+    tpl.Property("filter", FilterGetter, FilterSetter);
 
     tpl.StaticLazyProperty("local", LocalGetter);
 
