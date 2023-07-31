720e9259c09e7d3c1e880a01110c2036859305b5
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-07-31 05:16:56 +0200

    shared: Add value serializer / deserializer

---
+++ shared/src/helpers/ValueBuffer.cpp
@@ -0,0 +1,387 @@
+#include "ValueBuffer.h"
+#include "interfaces/IResource.h"
+#include "JS.h"
+
+bool js::IValueBuffer::Value(v8::Local<v8::Value>& value)
+{
+    ValueType type = GetValueType(value);
+    if(!Type(type)) return false;
+    switch(type)
+    {
+        case ValueType::JS_VALUE: return JSValue(value);
+        case ValueType::ENTITY: return Entity(value);
+        case ValueType::VECTOR3: return Vector3(value);
+        case ValueType::VECTOR2: return Vector2(value);
+        case ValueType::RGBA: return RGBA(value);
+        case ValueType::QUATERNION: return Quaternion(value);
+        case ValueType::BUFFER: return JSBuffer(value);
+    }
+    return false;
+}
+
+// *** Writer
+
+bool js::ValueSerializer::Buffer(void*& data, size_t size)
+{
+    serializer->WriteRawBytes(data, size);
+    return true;
+}
+
+bool js::ValueSerializer::Byte(uint8_t& value)
+{
+    serializer->WriteRawBytes(&value, sizeof(value));
+    return true;
+}
+
+bool js::ValueSerializer::Type(ValueType& value)
+{
+    uint8_t type = static_cast<uint8_t>(value);
+    serializer->WriteRawBytes(&type, sizeof(type));
+    return true;
+}
+
+bool js::ValueSerializer::Int32(int32_t& value)
+{
+    serializer->WriteRawBytes(&value, sizeof(value));
+    return true;
+}
+
+bool js::ValueSerializer::UInt32(uint32_t& value)
+{
+    serializer->WriteRawBytes(&value, sizeof(value));
+    return true;
+}
+
+bool js::ValueSerializer::Int64(int64_t& value)
+{
+    serializer->WriteRawBytes(&value, sizeof(value));
+    return true;
+}
+
+bool js::ValueSerializer::UInt64(uint64_t& value)
+{
+    serializer->WriteRawBytes(&value, sizeof(value));
+    return true;
+}
+
+bool js::ValueSerializer::Float(float& value)
+{
+    serializer->WriteRawBytes(&value, sizeof(value));
+    return true;
+}
+
+bool js::ValueSerializer::Double(double& value)
+{
+    serializer->WriteRawBytes(&value, sizeof(value));
+    return true;
+}
+
+bool js::ValueSerializer::JSValue(v8::Local<v8::Value>& value)
+{
+    v8::Maybe<bool> result = serializer->WriteValue(resource->GetContext(), value);
+    return result.FromMaybe(false);
+}
+
+bool js::ValueSerializer::Entity(v8::Local<v8::Value>& value)
+{
+    js::ScriptObject* scriptObject = resource->GetScriptObject(value);
+    if(!scriptObject) return false;
+    alt::IBaseObject* object = scriptObject->GetObject();
+    if(!object) return false;
+    uint8_t type = static_cast<uint8_t>(object->GetType());
+#ifdef ALT_CLIENT_API
+    uint32_t id = object->GetRemoteID();
+#else
+    uint32_t id = object->GetID();
+#endif
+
+    Byte(type);
+    UInt32(id);
+    return true;
+}
+
+bool js::ValueSerializer::Vector3(v8::Local<v8::Value>& value)
+{
+    js::Object obj = value.As<v8::Object>();
+    alt::Vector3f vector{ obj.Get<float>("x"), obj.Get<float>("y"), obj.Get<float>("z") };
+
+    Float(vector[0]);
+    Float(vector[1]);
+    Float(vector[2]);
+    return true;
+}
+
+bool js::ValueSerializer::Vector2(v8::Local<v8::Value>& value)
+{
+    js::Object obj = value.As<v8::Object>();
+    alt::Vector2f vector{ obj.Get<float>("x"), obj.Get<float>("y") };
+
+    Float(vector[0]);
+    Float(vector[1]);
+    return true;
+}
+
+bool js::ValueSerializer::RGBA(v8::Local<v8::Value>& value)
+{
+    js::Object obj = value.As<v8::Object>();
+    alt::RGBA rgba{ obj.Get<uint8_t>("r"), obj.Get<uint8_t>("g"), obj.Get<uint8_t>("b"), obj.Get<uint8_t>("a") };
+
+    Byte(rgba.r);
+    Byte(rgba.g);
+    Byte(rgba.b);
+    Byte(rgba.a);
+    return true;
+}
+
+bool js::ValueSerializer::Quaternion(v8::Local<v8::Value>& value)
+{
+    js::Object obj = value.As<v8::Object>();
+    alt::Quaternion quaternion{ obj.Get<float>("x"), obj.Get<float>("y"), obj.Get<float>("z"), obj.Get<float>("w") };
+
+    Float(quaternion.x);
+    Float(quaternion.y);
+    Float(quaternion.z);
+    Float(quaternion.w);
+    return true;
+}
+
+bool js::ValueSerializer::JSBuffer(v8::Local<v8::Value>& value)
+{
+    js::Object obj = value.As<v8::Object>();
+    js::Buffer* buffer = obj.GetExtraInternalFieldValue<js::Buffer>();
+    if(!buffer) return false;
+
+    size_t size = buffer->GetSize();
+    UInt64(size);
+    void* data = buffer->GetBuffer();
+    Buffer(data, size);
+    return true;
+}
+
+js::IValueBuffer::ValueType js::ValueSerializer::GetValueType(v8::Local<v8::Value> value)
+{
+    if(value.IsEmpty()) return ValueType::INVALID;
+    if(resource->IsBaseObject(value))
+    {
+        js::ScriptObject* scriptObject = resource->GetScriptObject(value);
+        if(!scriptObject) return ValueType::INVALID;
+        alt::IBaseObject* object = scriptObject->GetObject();
+        switch(object->GetType())
+        {
+            case alt::IBaseObject::Type::PLAYER:
+            case alt::IBaseObject::Type::LOCAL_PLAYER:
+            case alt::IBaseObject::Type::VEHICLE:
+            case alt::IBaseObject::Type::PED:
+            case alt::IBaseObject::Type::OBJECT: return ValueType::ENTITY;
+            default: return ValueType::INVALID;
+        }
+    }
+    else if(resource->IsVector3(value))
+        return ValueType::VECTOR3;
+    else if(resource->IsVector2(value))
+        return ValueType::VECTOR2;
+    else if(resource->IsRGBA(value))
+        return ValueType::RGBA;
+    else if(resource->IsQuaternion(value))
+        return ValueType::QUATERNION;
+    else if(resource->IsBuffer(value))
+        return ValueType::BUFFER;
+    else
+    {
+        js::Type type = js::GetType(value, nullptr);
+        switch(type)
+        {
+            case js::Type::FUNCTION:
+            case js::Type::SHARED_ARRAY_BUFFER:
+            case js::Type::PROMISE: return ValueType::INVALID;
+        }
+        return ValueType::JS_VALUE;
+    }
+
+    return ValueType::INVALID;
+}
+
+void js::ValueSerializer::ThrowDataCloneError(v8::Local<v8::String> message)
+{
+    js::Throw(CppValue(message));
+}
+
+v8::Maybe<bool> js::ValueSerializer::WriteHostObject(v8::Isolate* isolate, v8::Local<v8::Object> object)
+{
+    auto val = object.As<v8::Value>();
+    return v8::Just<bool>(Value(val));
+}
+
+std::optional<std::pair<uint8_t*, size_t>> js::ValueSerializer::Serialize(v8::Local<v8::Value> value, IResource* resource)
+{
+    js::ValueSerializer writer;
+    v8::ValueSerializer serializer(resource->GetIsolate(), &writer);
+    writer.resource = resource;
+    writer.serializer = &serializer;
+
+    writer.serializer->WriteHeader();
+    bool result = writer.Value(value);
+    if(!result) return std::nullopt;
+
+    return serializer.Release();
+}
+
+// *** Reader
+
+bool js::ValueDeserializer::Buffer(void*& data, size_t size)
+{
+    return deserializer->ReadRawBytes(size, (const void**)&data);
+}
+
+bool js::ValueDeserializer::Byte(uint8_t& value)
+{
+    return Read<uint8_t>(value);
+}
+
+bool js::ValueDeserializer::Type(ValueType& type)
+{
+    uint8_t typeVal = static_cast<uint8_t>(type);
+    bool result = Read<uint8_t>(typeVal);
+    if(!result) return false;
+    type = static_cast<ValueType>(typeVal);
+    return true;
+}
+
+bool js::ValueDeserializer::Int32(int32_t& value)
+{
+    return Read<int32_t>(value);
+}
+
+bool js::ValueDeserializer::UInt32(uint32_t& value)
+{
+    return Read<uint32_t>(value);
+}
+
+bool js::ValueDeserializer::Int64(int64_t& value)
+{
+    return Read<int64_t>(value);
+}
+
+bool js::ValueDeserializer::UInt64(uint64_t& value)
+{
+    return Read<uint64_t>(value);
+}
+
+bool js::ValueDeserializer::Float(float& value)
+{
+    return Read<float>(value);
+}
+
+bool js::ValueDeserializer::Double(double& value)
+{
+    return Read<double>(value);
+}
+
+bool js::ValueDeserializer::JSValue(v8::Local<v8::Value>& value)
+{
+    v8::MaybeLocal<v8::Value> result = deserializer->ReadValue(resource->GetContext());
+    if(result.IsEmpty()) return false;
+    value = result.ToLocalChecked();
+    return true;
+}
+
+bool js::ValueDeserializer::Entity(v8::Local<v8::Value>& value)
+{
+    uint8_t type;
+    if(!Byte(type)) return false;
+    uint32_t id;
+    if(!UInt32(id)) return false;
+#ifdef ALT_CLIENT_API
+    alt::IBaseObject* entity = alt::ICore::Instance().GetBaseObjectByRemoteID((alt::IBaseObject::Type)type, id);
+#else
+    alt::IBaseObject* entity = alt::ICore::Instance().GetBaseObjectByID((alt::IBaseObject::Type)type, id);
+#endif
+
+    if(!entity) return false;
+    value = resource->GetOrCreateScriptObject(resource->GetContext(), entity)->Get();
+    return true;
+}
+
+bool js::ValueDeserializer::Vector3(v8::Local<v8::Value>& value)
+{
+    float x, y, z;
+    if(!Float(x)) return false;
+    if(!Float(y)) return false;
+    if(!Float(z)) return false;
+    value = resource->CreateVector3({ x, y, z });
+    return true;
+}
+
+bool js::ValueDeserializer::Vector2(v8::Local<v8::Value>& value)
+{
+    float x, y;
+    if(!Float(x)) return false;
+    if(!Float(y)) return false;
+    value = resource->CreateVector2({ x, y });
+    return true;
+}
+
+bool js::ValueDeserializer::RGBA(v8::Local<v8::Value>& value)
+{
+    uint8_t r, g, b, a;
+    if(!Byte(r)) return false;
+    if(!Byte(g)) return false;
+    if(!Byte(b)) return false;
+    if(!Byte(a)) return false;
+    value = resource->CreateRGBA({ r, g, b, a });
+    return true;
+}
+
+bool js::ValueDeserializer::Quaternion(v8::Local<v8::Value>& value)
+{
+    float x, y, z, w;
+    if(!Float(x)) return false;
+    if(!Float(y)) return false;
+    if(!Float(z)) return false;
+    if(!Float(w)) return false;
+    value = resource->CreateQuaternion({ x, y, z, w });
+    return true;
+}
+
+extern js::Class bufferClass;
+bool js::ValueDeserializer::JSBuffer(v8::Local<v8::Value>& value)
+{
+    size_t size;
+    if(!UInt64(size)) return false;
+    uint8_t* data = new uint8_t[size];
+    void* dataPtr = data;
+    if(!Buffer(dataPtr, size))
+    {
+        delete[] data;
+        return false;
+    }
+    value = bufferClass.Create(resource->GetContext(), new js::Buffer(data, size, false));
+    return true;
+}
+
+v8::MaybeLocal<v8::Object> js::ValueDeserializer::ReadHostObject(v8::Isolate* isolate)
+{
+    v8::Local<v8::Value> value;
+    if(!Value(value)) return v8::MaybeLocal<v8::Object>();
+    return value.As<v8::Object>();
+}
+
+js::IValueBuffer::ValueType js::ValueDeserializer::GetValueType(v8::Local<v8::Value> value)
+{
+    return js::IValueBuffer::ValueType::INVALID;
+}
+
+std::optional<v8::Local<v8::Value>> js::ValueDeserializer::Deserialize(uint8_t* bytes, size_t size, IResource* resource)
+{
+    js::ValueDeserializer reader;
+    v8::ValueDeserializer deserializer(resource->GetIsolate(), bytes, size, &reader);
+    reader.resource = resource;
+    reader.deserializer = &deserializer;
+
+    if(!reader.deserializer->ReadHeader(resource->GetContext()).FromMaybe(false)) return std::nullopt;
+    v8::Local<v8::Value> value;
+    bool result = reader.Value(value);
+    if(!result) return std::nullopt;
+
+    return value;
+}
---
+++ shared/src/helpers/ValueBuffer.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <optional>
+
+#include "v8.h"
+
+namespace js
+{
+    class IResource;
+
+    class IValueBuffer
+    {
+    public:
+        enum class ValueType : uint8_t
+        {
+            INVALID,
+            JS_VALUE,
+            ENTITY,
+            VECTOR3,
+            VECTOR2,
+            RGBA,
+            QUATERNION,
+            BUFFER
+        };
+
+    protected:
+        IResource* resource = nullptr;
+
+        virtual bool Buffer(void*& data, size_t size) = 0;
+        virtual bool Byte(uint8_t& value) = 0;
+        virtual bool Type(ValueType& value) = 0;
+        virtual bool Int32(int32_t& value) = 0;
+        virtual bool UInt32(uint32_t& value) = 0;
+        virtual bool Int64(int64_t& value) = 0;
+        virtual bool UInt64(uint64_t& value) = 0;
+        virtual bool Float(float& value) = 0;
+        virtual bool Double(double& value) = 0;
+
+        virtual bool JSValue(v8::Local<v8::Value>& value) = 0;
+        virtual bool Entity(v8::Local<v8::Value>& value) = 0;
+        virtual bool Vector3(v8::Local<v8::Value>& value) = 0;
+        virtual bool Vector2(v8::Local<v8::Value>& value) = 0;
+        virtual bool RGBA(v8::Local<v8::Value>& value) = 0;
+        virtual bool Quaternion(v8::Local<v8::Value>& value) = 0;
+        virtual bool JSBuffer(v8::Local<v8::Value>& value) = 0;
+
+        virtual ValueType GetValueType(v8::Local<v8::Value> value) = 0;
+
+    public:
+        bool Value(v8::Local<v8::Value>& value);
+    };
+
+    class ValueSerializer : public virtual IValueBuffer, public v8::ValueSerializer::Delegate
+    {
+        v8::ValueSerializer* serializer = nullptr;
+
+        void ThrowDataCloneError(v8::Local<v8::String> message) override;
+        v8::Maybe<bool> WriteHostObject(v8::Isolate* isolate, v8::Local<v8::Object> object) override;
+
+        bool Buffer(void*& data, size_t size) override;
+        bool Byte(uint8_t& value) override;
+        bool Type(ValueType& value) override;
+        bool Int32(int32_t& value) override;
+        bool UInt32(uint32_t& value) override;
+        bool Int64(int64_t& value) override;
+        bool UInt64(uint64_t& value) override;
+        bool Float(float& value) override;
+        bool Double(double& value) override;
+
+        bool JSValue(v8::Local<v8::Value>& value) override;
+        bool Entity(v8::Local<v8::Value>& value) override;
+        bool Vector3(v8::Local<v8::Value>& value) override;
+        bool Vector2(v8::Local<v8::Value>& value) override;
+        bool RGBA(v8::Local<v8::Value>& value) override;
+        bool Quaternion(v8::Local<v8::Value>& value) override;
+        bool JSBuffer(v8::Local<v8::Value>& value) override;
+
+        ValueType GetValueType(v8::Local<v8::Value> value) override;
+
+    public:
+        static std::optional<std::pair<uint8_t*, size_t>> Serialize(v8::Local<v8::Value> value, IResource* resource);
+    };
+
+    class ValueDeserializer : public virtual IValueBuffer, public v8::ValueDeserializer::Delegate
+    {
+        v8::ValueDeserializer* deserializer = nullptr;
+
+        v8::MaybeLocal<v8::Object> ReadHostObject(v8::Isolate* isolate) override;
+
+        bool Buffer(void*& data, size_t size) override;
+        bool Byte(uint8_t& value) override;
+        bool Type(ValueType& value) override;
+        bool Int32(int32_t& value) override;
+        bool UInt32(uint32_t& value) override;
+        bool Int64(int64_t& value) override;
+        bool UInt64(uint64_t& value) override;
+        bool Float(float& value) override;
+        bool Double(double& value) override;
+
+        bool JSValue(v8::Local<v8::Value>& value) override;
+        bool Entity(v8::Local<v8::Value>& value) override;
+        bool Vector3(v8::Local<v8::Value>& value) override;
+        bool Vector2(v8::Local<v8::Value>& value) override;
+        bool RGBA(v8::Local<v8::Value>& value) override;
+        bool Quaternion(v8::Local<v8::Value>& value) override;
+        bool JSBuffer(v8::Local<v8::Value>& value) override;
+
+        ValueType GetValueType(v8::Local<v8::Value> value) override;
+
+        template<typename T>
+        bool Read(T& value)
+        {
+            void* ptr = nullptr;
+            if(!deserializer->ReadRawBytes(sizeof(T), (const void**)&ptr)) return false;
+            value = *(T*)ptr;
+            return true;
+        }
+
+    public:
+        static std::optional<v8::Local<v8::Value>> Deserialize(uint8_t* bytes, size_t size, IResource* resource);
+    };
+}  // namespace js
