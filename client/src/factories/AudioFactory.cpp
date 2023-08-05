827c389eebc3c38332d24a82b30083d027f281e9
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-08-06 01:04:34 +0200

    client: Fix audio factory

--- client/src/factories/AudioFactory.cpp
+++ client/src/factories/AudioFactory.cpp
@@ -9,5 +9,8 @@ static js::FactoryHandler audioFactory(alt::IBaseObject::Type::AUDIO, [](js::Obj
     float volume;
     if(!args.Get("volume", volume)) return nullptr;
 
-    return alt::ICore::Instance().CreateAudio(source, volume, args.GetResource()->GetResource());
+    bool isRadio = args.Get<bool>("isRadio", false);
+
+    js::IResource* resource = args.GetResource();
+    return alt::ICore::Instance().CreateAudio(source, volume, isRadio, js::SourceLocation::GetCurrent(resource).file, resource->GetResource());
 });
