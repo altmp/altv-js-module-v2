3530e26f6515e4c194b67180d3c7ee2276cdf8a2
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-09-09 16:58:53 +0200

    shared: Fix vehicle factory (closes #72)

--- shared/src/modules/SharedCppBindingsModule.cpp
+++ shared/src/modules/SharedCppBindingsModule.cpp
@@ -60,16 +60,6 @@ static void CreateEntity(js::FunctionContext& ctx)
     js::Object args;
     if(!ctx.GetArg(1, args)) return;
 
-    js::TryCatch tryCatch;
-    alt::IBaseObject* object = js::FactoryHandler::Create(type, args);
-    if(!object)
-    {
-        if(tryCatch.HasCaught()) tryCatch.ReThrow();
-        else
-            ctx.Throw("Failed to create entity of type " + std::string(magic_enum::enum_name(type)));
-        return;
-    }
-
     js::IResource* resource = ctx.GetResource();
     js::ScriptObject* scriptObject = nullptr;
     {
