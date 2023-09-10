d6b37854dc15a5da39511b57e8e38f9323631267
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-09-10 02:37:04 +0200

    Fix build

--- shared/src/modules/CompatibilityModule.cpp
+++ shared/src/modules/CompatibilityModule.cpp
@@ -2,4 +2,4 @@
 #include "interfaces/IResource.h"
 
 static js::Module compatibilityModule("alt", [](js::IResource* resource) -> v8::Local<v8::Object> { return resource->GetCompatibilityModuleNamespace(); });
-static js::Module compatibilityModule("alt-shared", [](js::IResource* resource) -> v8::Local<v8::Object> { return resource->GetCompatibilityModuleNamespace(); });
+static js::Module compatibilityModule2("alt-shared", [](js::IResource* resource) -> v8::Local<v8::Object> { return resource->GetCompatibilityModuleNamespace(); });
