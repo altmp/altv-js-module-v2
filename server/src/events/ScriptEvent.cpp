b64c8fcd0cabc3060bfb66758ae6bed69ab26536
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-09-11 22:46:06 +0200

    shared: make COLSHAPE_EVENT event shared

--- server/src/events/ScriptEvent.cpp
+++ server/src/events/ScriptEvent.cpp
@@ -1,15 +1,6 @@
 #include "Event.h"
 
 // clang-format off
-static js::Event colshapeEvent(alt::CEvent::Type::COLSHAPE_EVENT, [](const alt::CEvent* ev, js::Event::EventArgs& args)
-{
-    auto e = static_cast<const alt::CColShapeEvent*>(ev);
-
-    args.Set("entity", e->GetEntity());
-    args.Set("colShape", e->GetTarget());
-    args.Set("state", e->GetState());
-});
-
 static js::Event explosionEvent(alt::CEvent::Type::EXPLOSION_EVENT, [](const alt::CEvent* ev, js::Event::EventArgs& args)
 {
     auto e = static_cast<const alt::CExplosionEvent*>(ev);
--- shared/src/events/ScriptEvent.cpp
+++ shared/src/events/ScriptEvent.cpp
@@ -1,6 +1,16 @@
 #include "Event.h"
 
 // clang-format off
+
+static js::Event colshapeEvent(alt::CEvent::Type::COLSHAPE_EVENT, [](const alt::CEvent* ev, js::Event::EventArgs& args)
+{
+    auto e = static_cast<const alt::CColShapeEvent*>(ev);
+
+    args.Set("entity", e->GetEntity());
+    args.Set("colShape", e->GetTarget());
+    args.Set("state", e->GetState());
+});
+
 static js::Event clientScriptEvent(alt::CEvent::Type::CLIENT_SCRIPT_EVENT, [](const alt::CEvent* ev, js::Event::EventArgs& args)
 {
     auto e = static_cast<const alt::CClientScriptEvent*>(ev);
