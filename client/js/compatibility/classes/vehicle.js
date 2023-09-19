83d503e7d0658b1ae629c5a5fb9fea16947fdcb3
Stanislovas Janonis <stanislovas.janonis@gmail.com>
2023-09-20 02:12:40 +0300

    client: cleanup vehicle v1

--- client/js/compatibility/classes/vehicle.js
+++ client/js/compatibility/classes/vehicle.js
@@ -89,10 +89,6 @@ class Vehicle extends alt.Vehicle {
         return super.isDestroyed;
     }
 
-    get passengers() {
-        throw new Error("Not implemented");
-    }
-
     get engineOn() {
         return super.isEngineOn;
     }
@@ -135,115 +131,6 @@ class Vehicle extends alt.Vehicle {
 
     get rearWheels() {
         return super.rearWheelVariation;
-    }
-
-    get driftModeEnabled() {
-        return super.driftMode;
-    }
-
-    set driftModeEnabled(value) {
-        super.driftMode = value;
-    }
-
-    // constructor(model | number, x, y, z, rx, ry, rz);
-    // constructor(model | number, pos: shared.IVector3, rot: shared.IVector3);
-    constructor(model, ...args) {
-        const pos = args.length === 2 ? args[1] : { x: args[1], y: args[2], z: args[3] };
-        const rot = args.length === 2 ? args[2] : { x: args[4], y: args[5], z: args[6] };
-
-        return super.create(model, pos, rot);
-    }
-
-    getAppearanceDataBase64() {
-        return super.appearanceDataBase64;
-    }
-
-    getDamageStatusBase64() {
-        return super.damageDataBase64;
-    }
-
-    getExtra(extraId) {
-        return super.isExtraOn(extraId);
-    }
-
-    getGamestateDataBase64() {
-        return super.gameStateDataBase64;
-    }
-
-    getHealthDataBase64() {
-        return super.healthDataBase64;
-    }
-
-    getScriptDataBase64() {
-        return super.scriptDataBase64;
-    }
-
-    setAppearanceDataBase64(data) {
-        super.appearanceDataBase64 = data;
-    }
-
-    setDamageStatusBase64(data) {
-        super.damageDataBase64 = data;
-    }
-
-    setExtra(extraId, state) {
-        super.toggleExtra(extraId, state);
-    }
-
-    setGamestateDataBase64(data) {
-        super.gameStateDataBase64 = data;
-    }
-
-    setHealthDataBase64(data) {
-        super.healthDataBase64 = data;
-    }
-
-    setScriptDataBase64(data) {
-        super.scriptDataBase64 = data;
-    }
-
-    get trainEngineId() {
-        return super.trainEngine;
-    }
-
-    setTrainEngineId(vehicle) {
-        super.trainEngine = vehicle;
-    }
-
-    get trainLinkedToBackwardId() {
-        return super.trainLinkedToBackward;
-    }
-
-    setTrainLinkedToBackwardId(vehicle) {
-        super.trainLinkedToBackward = vehicle;
-    }
-
-    get trainLinkedToForwardId() {
-        return super.trainLinkedToForward;
-    }
-
-    setTrainLinkedToForwardId(vehicle) {
-        super.trainLinkedToForward = vehicle;
-    }
-
-    setSearchLightTo(entity, state) {
-        super.setSearchLight(entity, state);
-    }
-
-    get boatAnchorActive() {
-        return super.isBoatAnchorActive;
-    }
-
-    set boatAnchorActive(value) {
-        super.isBoatAnchorActive = value;
-    }
-
-    get hornActive() {
-        return super.isHornActive;
-    }
-
-    set hornActive(value) {
-        super.isHornActive = value;
     }
 
     setMeta(key, value) {
