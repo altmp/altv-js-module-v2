ec1968bd88ad2761ed8a8152a17c14fdb32c5d52
Yiin <stanislovas.janonis@gmail.com>
2023-09-20 04:43:04 +0300

    shared: fix types & add missing blip enums (#86)
    * shared: move event handler typings (#85)
    
    * webview: fix types
    
    * shared: add BlipColor and BlipSprite enums

--- types/webview/index.d.ts
+++ types/webview/index.d.ts
@@ -1,5 +1,3 @@
-import altShared from "@altv/shared";
-
 interface Alt {
     /**
      * Emits specified event across particular client.
@@ -7,7 +5,8 @@ interface Alt {
      * @param eventName Name of the event.
      * @param args Rest parameters for emit to send.
      */
-    emit<E extends keyof altShared.Events.CustomWebViewToClientEvent>(eventName: E, ...args: Parameters<altShared.Events.CustomWebViewToClientEvent[E]>): void;
+    emit<E extends keyof import("@altv/shared").Events.CustomWebViewToClientEvent>(eventName: E, ...args: Parameters<import("@altv/shared").Events.CustomWebViewToClientEvent[E]>): void;
+    emit<E extends string>(eventName: Exclude<E, keyof import("@altv/shared").Events.CustomWebViewToClientEvent>, ...args: unknown[]): void;
 
     /**
      * Unsubscribes from client event handler with specified listener.
@@ -17,7 +16,8 @@ interface Alt {
      * @param eventName Name of the event.
      * @param listener Listener that should be removed.
      */
-    off<E extends keyof altShared.Events.CustomWebViewToClientEvent>(eventName: E, listener: altShared.Events.CustomWebViewToClientEvent[E]): void;
+    off<E extends keyof import("@altv/shared").Events.CustomWebViewToClientEvent>(eventName: E, listener: import("@altv/shared").Events.CustomWebViewToClientEvent[E]): void;
+    off<E extends string>(eventName: Exclude<E, keyof import("@altv/shared").Events.CustomWebViewToClientEvent>, listener: (...args: unknown[]) => void): void;
 
     /**
      * Subscribes to client event handler with specified listener.
@@ -25,7 +25,8 @@ interface Alt {
      * @param eventName Name of the event.
      * @param listener Listener that should be added.
      */
-    on<E extends keyof altShared.Events.CustomClientToWebViewEvent>(eventName: E, listener: altShared.Events.CustomClientToWebViewEvent[E]): void;
+    on<E extends keyof import("@altv/shared").Events.CustomClientToWebViewEvent>(eventName: E, listener: import("@altv/shared").Events.CustomClientToWebViewEvent[E]): void;
+    on<E extends string>(eventName: Exclude<E, keyof import("@altv/shared").Events.CustomClientToWebViewEvent>, listener: (...args: unknown[]) => void): void;
 
     /**
      * Subscribes to client event with specified listener, which only triggers once.
@@ -33,15 +34,10 @@ interface Alt {
      * @param eventName Name of the event.
      * @param listener Listener that should be added.
      */
-    once<E extends keyof altShared.Events.CustomClientToWebViewEvent>(eventName: E, listener: altShared.Events.CustomClientToWebViewEvent[E]): void;
+    once<E extends keyof import("@altv/shared").Events.CustomClientToWebViewEvent>(eventName: E, listener: import("@altv/shared").Events.CustomClientToWebViewEvent[E]): void;
+    once<E extends string>(eventName: Exclude<E, keyof import("@altv/shared").Events.CustomClientToWebViewEvent>, listener: (...args: unknown[]) => void): void;
 
-    /**
-     * Gets all the listeners for the specified webview event.
-     *
-     * @param eventName Name of the event.
-     * @returns Array of listener functions for that event.
-     */
-    getEventListeners<E extends keyof altShared.Events.CustomClientToWebViewEvent>(eventName: E): (altShared.Events.CustomClientToWebViewEvent[E])[];
+    readonly listeners: Readonly<Record<string, ((...args: unknown[]) => void)[]>>;
 
     getVersion(): string;
     getBranch(): string;
