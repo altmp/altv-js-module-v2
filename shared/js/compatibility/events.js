6b33b9b1432cca11a9b366a0c7de16346174b9f3
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-09-30 15:03:20 +0200

    server: add compatr emitClient methods

--- shared/js/compatibility/events.js
+++ shared/js/compatibility/events.js
@@ -113,7 +113,54 @@ alt.Events.onEvent(async (ctx) => {
     }
 });
 
+function emitClient(player, eventName, ...args) {
+    if (!player instanceof alt.Player || !Array.isArray(player)) return;
+
+    if (Array.isArray(player)) {
+        alt.Events.emitPlayers(player, eventName, ...args);
+        return;
+    }
+
+    player.emit(eventName, ...args);
+}
+
+function emitClientRaw(player, eventName, ...args) {
+    if (!player instanceof alt.Player || !Array.isArray(player)) return;
+
+    if (Array.isArray(player)) {
+        alt.Events.emitPlayersRaw(player, eventName, ...args);
+        return;
+    }
+
+    player.emitRaw(eventName, ...args);
+}
+
+function emitClientUnreliable(player, eventName, ...args) {
+    if (!player instanceof alt.Player || !Array.isArray(player)) return;
+
+    if (Array.isArray(player)) {
+        alt.Events.emitPlayersUnreliable(player, eventName, ...args);
+        return;
+    }
+
+    player.emitUnreliable(eventName, ...args);
+}
+
 cppBindings.registerCompatibilityExport("on", on);
 cppBindings.registerCompatibilityExport("once", once);
 cppBindings.registerCompatibilityExport("off", off);
 cppBindings.registerCompatibilityExport("getEventListeners", getEventListeners);
+
+cppBindings.registerCompatibilityExport("emitClient", emitClient);
+cppBindings.registerCompatibilityExport("emitClientRaw", emitClientRaw);
+cppBindings.registerCompatibilityExport("emitClientUnreliable", emitClientUnreliable);
+
+cppBindings.registerCompatibilityExport("emitAllClients", alt.Events.emitAllPlayers);
+cppBindings.registerCompatibilityExport("emitAllClientsRaw", alt.Events.emitAllPlayersRaw);
+cppBindings.registerCompatibilityExport("emitAllClientsUnreliable", alt.Events.emitAllPlayersUnreliableRaw);
+
+// TODO (xLuxy)
+// export function offClient(eventName: string, listener: (player: Player, ...args: any[]) => void): void;
+// export function offClient(listener: (eventName: string, ...args: any[]) => void): void;
+// export function onClient(listener: (eventName: string, player: Player, ...args: any[]) => void): void;
+// export function onceClient(listener: (eventName: string, player: Player, ...args: any[]) => void): void;
