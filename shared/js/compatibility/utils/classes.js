1b6eea4ceb8a4250c33ceea37f09a0aab5377436
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-10-03 08:05:10 +0200

    shared: improve extendAltEntityClass by not allowing re-assignments

--- shared/js/compatibility/utils/classes.js
+++ shared/js/compatibility/utils/classes.js
@@ -1,22 +1,31 @@
 /// <reference path="../../../../types/shared/index.d.ts" />
 // import * as alt from "@altv/shared";
 
+requireBinding("shared/logging.js");
+
 /** @type {typeof import("../../../../shared/js/utils.js")} */
-const { assert, assertIsType } = requireBinding("shared/utils.js");
+const { assertIsType } = requireBinding("shared/utils.js");
 
 export function extendAltEntityClass(baseInstance, ...classes) {
     if (baseInstance == null || typeof baseInstance !== "object") return;
 
     for (const class_ of classes) {
-        assertIsType(class_, "function", `Expected a class object as the first argument, but got ${typeof class_}`);
+        assertIsType(class_, "function", `Expected class object, but got ${typeof class_}`);
 
         for (const propKey of Object.getOwnPropertyNames(class_.prototype)) {
             if (propKey === "constructor") continue;
 
-            const descriptor = Object.getOwnPropertyDescriptor(class_.prototype, propKey);
-            if (descriptor) {
-                Object.defineProperty(baseInstance, propKey, descriptor);
+            // TODO (xLuxy): We might want to override shared properties?
+            if (baseInstance.hasOwnProperty(propKey)) {
+                const baseClassName = baseInstance.constructor.name;
+                const className = class_.name;
+
+                alt.log(`~lr~[Compatibility] Property '${propKey}' skipped in '${className}' - already in base class '${baseClassName}'.`);
+                continue;
             }
+
+            const descriptor = Object.getOwnPropertyDescriptor(class_.prototype, propKey);
+            if (descriptor) Object.defineProperty(baseInstance, propKey, descriptor);
         }
     }
 
