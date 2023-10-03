56ba7e7cc6fcd930f0a3bf9f10377f2be22db8c6
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-10-03 07:17:52 +0200

    server: Add base compatiblity Vehicle class

---
+++ server/js/compatibility/classes/vehicle.js
@@ -0,0 +1,29 @@
+/// <reference path="../../../../types/shared/index.d.ts" />
+/// <reference path="../../../../types/server/index.d.ts" />
+// import * as alt from "@altv/server";
+
+requireBinding("shared/entity.js");
+
+/** @type {typeof import("../../../../shared/js/utils.js")} */
+const { assertIsType, assertIsObject } = requireBinding("shared/utils.js");
+
+const { Entity } = requireBinding("server/compatibility/classes/entity.js");
+const { extendAltEntityClass } = requireBinding("shared/compatibility/utils/classes.js");
+
+class Vehicle extends alt.Vehicle {
+    constructor(...args) {
+        // NOTE (xLuxy): This prevents the infinite loop caused by alt.*.create
+        if (!args.length) return super();
+
+        const [model, ...rest] = args;
+        const pos = rest.length <= 3 ? rest[0] : { x: rest[0], y: rest[1], z: rest[2] };
+        const rot = rest.length <= 3 ? rest[1] : { x: rest[3], y: rest[4], z: rest[5] };
+        const streamingDistance = rest.length === 3 ? rest[2] : rest[6];
+
+        const instance = alt.Vehicle.create({ model, pos, rot, streamingDistance });
+        return extendAltEntityClass(instance, Entity);
+    }
+}
+
+alt.Factory.setVehicleFactory(Vehicle);
+cppBindings.registerCompatibilityExport("Vehicle", Vehicle);
