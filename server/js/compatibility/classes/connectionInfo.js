2c3915ae1c815cf64e1b69f33765d3ae2fc0f564
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-10-06 16:19:10 +0200

    server: Add ConnectionInfo compat class

---
+++ server/js/compatibility/classes/connectionInfo.js
@@ -0,0 +1,33 @@
+/// <reference path="../../../../types/shared/index.d.ts" />
+/// <reference path="../../../../types/server/index.d.ts" />
+import * as alt from "@altv/server";
+
+requireBinding("shared/entity.js");
+
+class ConnectionInfo extends alt.ConnectionInfo {
+    get debug() {
+        return super.isDebug;
+    }
+
+    get socialClubName() {
+        return super.socialName;
+    }
+
+    // NOTE (xLuxy): In v1, value was returned as string for some reason
+    get passwordHash() {
+        return super.passwordHash.toString();
+    }
+
+    // NOTE (xLuxy): In v1, value was returned as string for some reason
+    get hwidHash() {
+        return super.hwidHash.toString();
+    }
+
+    // NOTE (xLuxy): In v1, value was returned as string for some reason
+    get hwidExHash() {
+        return super.hwidExHash.toString();
+    }
+}
+
+alt.Factory.setConnectionInfoFactory(ConnectionInfo);
+cppBindings.registerCompatibilityExport("ConnectionInfo", ConnectionInfo);
