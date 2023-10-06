95ed2e4dc0631d909d9f328b79e53fe23d4cb27c
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-10-06 16:27:55 +0200

    shared: Add ConnectDeniedReason enum

--- types/shared/index.d.ts
+++ types/shared/index.d.ts
@@ -2714,6 +2714,14 @@ declare module "@altv/shared" {
             LIMO,
             GREEN
         }
+
+        export enum ConnectDeniedReason {
+            WRONG_VERSION,
+            WRONG_BRANCH,
+            DEBUG_NOT_ALLOWED,
+            WRONG_PASSWORD,
+            WRONG_CDN_URL
+        }
     }
 
     export namespace Symbols {
