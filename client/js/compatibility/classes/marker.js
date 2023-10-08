cd23ae0607641bde778d61fa716db3e1b591b75d
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-10-08 23:40:35 +0200

    shared: Add Marker compatibility

---
+++ client/js/compatibility/classes/marker.js
@@ -0,0 +1,25 @@
+/// <reference path="../../../../types/shared/index.d.ts" />
+/// <reference path="../../../../types/client/index.d.ts" />
+// import * as alt from "@altv/client";
+
+requireBinding("shared/entity.js");
+
+const { WorldObject } = requireBinding("client/compatibility/classes/worldObject.js");
+const { BaseObject } = requireBinding("client/compatibility/classes/baseObject.js");
+
+const { extendAltEntityClass } = requireBinding("shared/compatibility/utils/classes.js");
+
+class Marker extends alt.Marker {
+    constructor(...args) {
+        // NOTE (xLuxy): This prevents the infinite loop caused by alt.*.create
+        if (!args.length) return super();
+
+        const [type, pos, color, useStreaming, streamingDistance] = args;
+
+        const instance = alt.Marker.create({ type, pos, color, useStreaming, streamingDistance });
+        return extendAltEntityClass(instance, WorldObject, BaseObject);
+    }
+}
+
+alt.Factory.setMarkerFactory(Marker);
+cppBindings.registerCompatibilityExport("Marker", Marker);
---
+++ server/js/compatibility/classes/marker.js
@@ -0,0 +1,25 @@
+/// <reference path="../../../../types/shared/index.d.ts" />
+/// <reference path="../../../../types/server/index.d.ts" />
+// import * as alt from "@altv/server";
+
+requireBinding("shared/entity.js");
+
+const { WorldObject } = requireBinding("server/compatibility/classes/worldObject.js");
+const { BaseObject } = requireBinding("server/compatibility/classes/baseObject.js");
+
+const { extendAltEntityClass } = requireBinding("shared/compatibility/utils/classes.js");
+
+class Marker extends alt.Marker {
+    constructor(...args) {
+        // NOTE (xLuxy): This prevents the infinite loop caused by alt.*.create
+        if (!args.length) return super();
+
+        const [type, pos, color] = args;
+
+        const instance = alt.Marker.create({ type, pos, color });
+        return extendAltEntityClass(instance, WorldObject, BaseObject);
+    }
+}
+
+alt.Factory.setMarkerFactory(Marker);
+cppBindings.registerCompatibilityExport("Marker", Marker);
