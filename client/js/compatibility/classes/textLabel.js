b7efd1cfc5d647ac7d24ab09998344054d5702c6
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-10-09 11:39:53 +0200

    client: TextLabel compatibility

---
+++ client/js/compatibility/classes/textLabel.js
@@ -0,0 +1,25 @@
+/// <reference path="../../../../types/shared/index.d.ts" />
+/// <reference path="../../../../types/client/index.d.ts" />
+// import * as alt from "@altv/client";
+
+requireBinding("shared/factory.js");
+
+const { WorldObject } = requireBinding("client/compatibility/classes/worldObject.js");
+const { BaseObject } = requireBinding("client/compatibility/classes/baseObject.js");
+
+const { extendAltEntityClass } = requireBinding("shared/compatibility/utils/classes.js");
+
+class TextLabel extends alt.TextLabel {
+    constructor(...args) {
+        // NOTE (xLuxy): This prevents the infinite loop caused by alt.*.create
+        if (!args.length) return super();
+
+        const [text, fontName, fontSize, fontScale, pos, rot, color, outlineWidth, outlineColor, useStreaming, streamingDistance] = args;
+
+        const instance = alt.TextLabel.create({ text, fontName, fontSize, fontScale, pos, rot, color, outlineWidth, outlineColor, useStreaming, streamingDistance });
+        return extendAltEntityClass(instance, WorldObject, BaseObject);
+    }
+}
+
+alt.Factory.setTextLabelFactory(TextLabel);
+cppBindings.registerCompatibilityExport("TextLabel", TextLabel);
