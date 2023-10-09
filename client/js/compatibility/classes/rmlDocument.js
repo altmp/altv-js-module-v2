d18876ef742b3d04dbb0e566ef899d693306c201
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-10-09 05:04:13 +0200

    client: Add RmlDocument and RmlElement compatibility

---
+++ client/js/compatibility/classes/rmlDocument.js
@@ -0,0 +1,26 @@
+/// <reference path="../../../../types/shared/index.d.ts" />
+/// <reference path="../../../../types/client/index.d.ts" />
+// import * as alt from "@altv/client";
+
+requireBinding("shared/factory.js");
+
+const { BaseObject } = requireBinding("client/compatibility/classes/baseObject.js");
+
+const { extendAltEntityClass } = requireBinding("shared/compatibility/utils/classes.js");
+
+class RmlDocument extends alt.RmlDocument {
+    constructor(...args) {
+        // NOTE (xLuxy): This prevents the infinite loop caused by alt.*.create
+        if (!args.length) return super();
+
+        const [url] = args;
+
+        const instance = alt.RmlDocument.create({ url });
+
+        // TODO (xLuxy): Might need to also extend RmlElement manually
+        return extendAltEntityClass(instance, BaseObject);
+    }
+}
+
+alt.Factory.setRmlDocumentFactory(RmlDocument);
+cppBindings.registerCompatibilityExport("RmlDocument", RmlDocument);
---
+++ client/js/compatibility/classes/rmlElement.js
@@ -0,0 +1,47 @@
+/// <reference path="../../../../types/shared/index.d.ts" />
+/// <reference path="../../../../types/client/index.d.ts" />
+// import * as alt from "@altv/client";
+
+requireBinding("shared/entity.js");
+
+const { BaseObject } = requireBinding("client/compatibility/classes/baseObject.js");
+
+const { extendAltEntityClass } = requireBinding("shared/compatibility/utils/classes.js");
+
+class RmlElement extends alt.RmlElement {
+    constructor(...args) {
+        super(...args);
+
+        extendAltEntityClass(this, BaseObject);
+    }
+
+    getEventListeners() {
+        return super.listeners;
+    }
+
+    get rmlId() {
+        return super.rmlID;
+    }
+
+    set rmlId(value) {
+        super.rmlID = value;
+    }
+
+    getClassList() {
+        return super.classList;
+    }
+
+    getPseudoClassList() {
+        return super.pseudoClassList;
+    }
+
+    getAttributes() {
+        return super.attributes;
+    }
+
+    closest() {
+        return super.getClosest();
+    }
+}
+
+cppBindings.registerCompatibilityExport("RmlElement", RmlElement);
