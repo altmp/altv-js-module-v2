09c93dbc2a819dedd5fdeac41e3e53199b1104af
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-10-09 07:13:02 +0200

    shared: Add Checkpoint compatibility

--- client/js/compatibility/classes/checkpoint.js
+++ client/js/compatibility/classes/checkpoint.js
@@ -1,11 +1,11 @@
 /// <reference path="../../../../types/shared/index.d.ts" />
-/// <reference path="../../../../types/server/index.d.ts" />
-// import * as alt from "@altv/server";
+/// <reference path="../../../../types/client/index.d.ts" />
+// import * as alt from "@altv/client";
 
 requireBinding("shared/factory.js");
 
-const { BaseObject } = requireBinding("server/compatibility/classes/baseObject.js");
-const { WorldObject } = requireBinding("server/compatibility/classes/worldObject.js");
+const { BaseObject } = requireBinding("client/compatibility/classes/baseObject.js");
+const { WorldObject } = requireBinding("client/compatibility/classes/worldObject.js");
 
 const { extendAltEntityClass } = requireBinding("shared/compatibility/utils/classes.js");
 
