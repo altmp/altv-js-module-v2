340ae4e2f59a4adbbbdc4a46430a491fa579b2f6
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-10-09 09:43:12 +0200

    client: Add LocalPlayer compat

---
+++ client/js/compatibility/classes/localPlayer.js
@@ -0,0 +1,9 @@
+/// <reference path="../../../../types/shared/index.d.ts" />
+/// <reference path="../../../../types/client/index.d.ts" />
+// import * as alt from "@altv/client";
+
+class LocalPlayer extends alt.Player {
+    //
+}
+
+cppBindings.registerCompatibilityExport("LocalPlayer", alt.LocalPlayer);
