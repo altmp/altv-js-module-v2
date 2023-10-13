b88187e668574415b7fb8e9a9fc735e1dfb2ed17
xLuxy <67131061+xLuxy@users.noreply.github.com>
2023-10-14 00:35:04 +0200

    shared: Add File compat class

---
+++ shared/js/compatibility/classes/file.js
@@ -0,0 +1,6 @@
+/// <reference path="../../../../types/shared/index.d.ts" />
+/// <reference path="../../../../types/server/index.d.ts" />
+/// <reference path="../../../../types/client/index.d.ts" />
+// import * as alt from "@altv/shared";
+
+cppBindings.registerCompatibilityExport("File", alt.File);
