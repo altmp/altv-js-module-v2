56f37f6fa46fdaad5983bd02cbf3217f6fdb21a5
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-10-19 01:12:56 +0200

    shared: Custom stack trace to hide internals in production

---
+++ shared/js/errors.js
@@ -0,0 +1,33 @@
+// Custom stack trace handling
+
+Error.showInternals = false;
+
+function isValidFrame(frame) {
+    const showInternals = Error.showInternals || alt.isDebug;
+    return !(!showInternals && frame.getFileName().startsWith("internal:"));
+}
+
+/**
+ * @param {Error} error
+ * @param {({})[]} frames
+ * @returns
+ */
+Error.prepareStackTrace = function (error, frames) {
+    let formatted = "";
+
+    let errorStr;
+    try {
+        errorStr = error.toString();
+    } catch (e) {
+        errorStr = `<error>`;
+    }
+    formatted += errorStr;
+
+    for (let i = 0; i < frames.length; i++) {
+        const frame = frames[i];
+        if (!isValidFrame(frame)) continue;
+        formatted += `\n    at ${frame.toString()}`;
+    }
+
+    return formatted;
+};
