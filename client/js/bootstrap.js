2ac9f4157c91b46680a27c244b346a1f9dfd9ccd
LeonMrBonnie <leonmrbonnie@hotmail.com>
2023-10-21 01:10:34 +0200

    client: Remove unused bootstrapper

--- client/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-const alt = __altModule;
-alt.log("Bootstrapper loaded", alt.Resource.current.name);
-
-try {
-    __startResource();
-} catch (err) {
-    alt.logError("Error while starting resource", alt.Resource.current.name);
-    alt.logError(err);
-}
