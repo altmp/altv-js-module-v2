f79591b8c382d8e502d1877ae22be75e4bf8ddf8
Yiin <stanislovas.janonis@gmail.com>
2023-10-24 11:50:14 +0300

    shared: add normalize() compat to Vector2

--- shared/js/compatibility/vector.js
+++ shared/js/compatibility/vector.js
@@ -1,3 +1,5 @@
+/// <reference path="../../../types/shared/index.d.ts" />
+
 requireBinding("shared/classes/vector.js");
 
 class Vector3 extends alt.Vector3 {
@@ -6,4 +8,11 @@ class Vector3 extends alt.Vector3 {
     }
 }
 
+class Vector2 extends alt.Vector2 {
+    normalize() {
+        return this.normalized;
+    }
+}
+
 cppBindings.registerCompatibilityExport("Vector3", Vector3);
+cppBindings.registerCompatibilityExport("Vector2", Vector2);
