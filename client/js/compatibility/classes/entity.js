d8b930279025a3abe9a91c0245520c13a0ae96b5
Yiin <stanislovas.janonis@gmail.com>
2023-10-29 21:13:19 +0200

    fix entity compatibilityu

--- client/js/compatibility/classes/entity.js
+++ client/js/compatibility/classes/entity.js
@@ -1,7 +1,10 @@
 /// <reference path="../../../../types/shared/index.d.ts" />
 /// <reference path="../../../../types/client/index.d.ts" />
+// import * as alt from "@altv/client";
 
 const { SharedEntity } = requireBinding("shared/compatibility/classes/sharedEntity.js");
+const { WorldObject } = requireBinding("client/compatibility/classes/worldObject.js");
+const { BaseObject } = requireBinding("client/compatibility/classes/baseObject.js");
 
 export class Entity extends alt.Entity {
     constructor() {
