95be684819f416b2786767a233b5f860c4387611
FlorianV85 <flopanofficiel@gmail.com>
2023-11-12 14:17:48 +0100

    shared: add `NONE` to `Enums.VehicleIndicatorLights`

--- shared/js/enums/vehicleIndicatorLights.js
+++ shared/js/enums/vehicleIndicatorLights.js
@@ -1,5 +1,6 @@
 // When changing, update types/shared/index.d.ts aswell
 alt.Enums.VehicleIndicatorLights = Object.freeze({
+    NONE: 0,
     BLINK_LEFT: 1,
     BLINK_RIGHT: 2,
     BLINK_PERM_BOTH: 4,
