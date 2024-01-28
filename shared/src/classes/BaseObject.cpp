c5fda66ff33cac7d47100d2edfc54ee26e042fe6
xLuxy <67131061+xLuxy@users.noreply.github.com>
2024-01-28 19:17:24 +0100

    fix(client): Fix ScriptObject not being disposed for RmlElement children

--- shared/src/classes/BaseObject.cpp
+++ shared/src/classes/BaseObject.cpp
@@ -1,9 +1,24 @@
 #include "Class.h"
 #include "cpp-sdk/ICore.h"
+#include "interfaces/IResource.h"
 
 static void ValidGetter(js::PropertyContext& ctx)
 {
     ctx.Return(ctx.GetThisObject<alt::IBaseObject>() != nullptr);
+}
+
+// NOTE(xLuxy): This is a workaround for alt:V not calling BaseObject for RmlElements
+static void RemoveRmlChildrens(alt::IRmlElement* element, js::IResource* resource)
+{
+    for (size_t i = 0; i < element->GetChildCount(); i++)
+    {
+        const auto children = element->GetChild(i);
+
+        resource->DestroyScriptObject(children);
+        RemoveRmlChildrens(children, resource);
+    }
+
+    alt::ICore::Instance().DestroyBaseObject(element);
 }
 
 static void Destroy(js::FunctionContext& ctx)
@@ -11,6 +26,14 @@ static void Destroy(js::FunctionContext& ctx)
     if(!ctx.CheckThis()) return;
 
     alt::IBaseObject* obj = ctx.GetThisObject<alt::IBaseObject>();
+
+    // TODO(xLuxy): alt:V currently doesn't create BaseObject for RmlElements
+    if (obj->GetType() == alt::IBaseObject::Type::RML_ELEMENT || obj->GetType() == alt::IBaseObject::Type::RML_DOCUMENT)
+    {
+        RemoveRmlChildrens(ctx.GetThisObject<alt::IRmlElement>(), ctx.GetResource());
+        ctx.GetResource()->DestroyScriptObject(obj);
+    }
+
     alt::ICore::Instance().DestroyBaseObject(obj);
 }
 
