692311f98496a6baf661d7380d568f83895df7a1
Yiin <stanislovas.janonis@gmail.com>
2024-02-19 00:35:19 +0200

    shared: add description merging in compatibility

--- shared/js/compatibility/utils/classes.js
+++ shared/js/compatibility/utils/classes.js
@@ -22,26 +22,36 @@ const defaultOptions = {
 
 function applyNonStaticProperties(baseClass, cls, options) {
     let prot = cls.prototype;
-    while (prot != Object.prototype) {
+    while (prot !== Object.prototype) {
         for (const prop of Object.getOwnPropertyNames(prot)) {
-            const doesPropertyExist = prop in baseClass || prop in baseClass.prototype;
-            const canBeOverriden = !options.blacklist.nonStatic.includes(prop) || options.whitelist.nonStatic.includes(prop);
+            const baseDescriptor = Object.getOwnPropertyDescriptor(baseClass.prototype, prop);
+            const newDescriptor = Object.getOwnPropertyDescriptor(prot, prop);
 
-            if (doesPropertyExist && !canBeOverriden) {
-                if (options.verbose) {
-                    const reason = !canBeOverriden ? "blacklisted" : "already exists";
-                    alt.log(`~ly~[JS] ~lb~Skipping non static property ${prop} in ${prot.constructor.name}: ${reason}`);
+            const isBlacklisted = options.blacklist.nonStatic.includes(prop);
+            const isWhitelisted = options.whitelist.nonStatic.includes(prop);
+            const canBeMerged = !isBlacklisted || isWhitelisted;
+
+            if (canBeMerged && newDescriptor) {
+                let mergedDescriptor = {};
+
+                if (baseDescriptor) {
+                    mergedDescriptor = { ...baseDescriptor };
                 }
 
-                continue;
-            }
+                ["get", "set", "value", "writeable"].forEach((key) => {
+                    if (key in newDescriptor && !mergedDescriptor[key]) {
+                        if (options.verbose) {
+                            alt.log(`~ly~[JS] ~lr~Merged ${key} for ${prop} from ${prot.constructor.name} to ${baseClass.name}`);
+                        }
+                        mergedDescriptor[key] = newDescriptor[key];
+                    }
+                });
 
-            const property = Object.getOwnPropertyDescriptor(prot, prop);
-            if (property) {
-                Object.defineProperty(baseClass.prototype, prop, property);
+                Object.defineProperty(baseClass.prototype, prop, mergedDescriptor);
 
                 if (options.verbose) {
-                    alt.log(`~ly~[JS] ~lb~Applied non static property ${prop} in ${prot.constructor.name} to ${baseClass.name}`);
+                    const action = baseDescriptor ? "Merged" : "Applied";
+                    alt.log(`~ly~[JS] ~lb~${action} non-static property ${prop} from ${prot.constructor.name} to ${baseClass.name}`);
                 }
             }
         }
