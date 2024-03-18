2f3bb19cda033b6273fa5bc3dd3480ce8418c917
xLuxy <67131061+xLuxy@users.noreply.github.com>
2024-03-18 13:42:33 +0100

    fix(client): bind httpClient instance to resource

--- client/src/factories/HttpClientFactory.cpp
+++ client/src/factories/HttpClientFactory.cpp
@@ -1,6 +1,9 @@
 ﻿#include "FactoryHandler.h"
+#include "interfaces/IResource.h"
 
 // clang-format off
 static js::FactoryHandler httpClientFactory(alt::IBaseObject::Type::HTTP_CLIENT, [](js::Object& args) -> alt::IBaseObject* {
-    return alt::ICore::Instance().CreateHttpClient();
+    alt::IResource* resource = args.GetResource()->GetResource();
+
+    return alt::ICore::Instance().CreateHttpClient(resource);
 });
